2a1d5b80189d6c881bac2747a35877e4696894ec
PremekPaska <premysl.paska@keenswh.com>
2015-06-29 17:01:20 +0200

    Update comment in pack_ptx_zip.cmd

--- Sources/Installer/InstallerAction/pack_ptx_zip.cmd
+++ Sources/Installer/InstallerAction/pack_ptx_zip.cmd
@@ -1,11 +1,11 @@
 ﻿rem $(ProjectDir) = %1, $(ConfigurationName) = %2
 
 rem NOTES:
-rem This script assumes that CUDAKernels have been build with the same configuration (Debug/Release) as this project
-rem And that CUDAKernels have been built before this project.
+rem This script assumes that CUDA kernels have been build with the same configuration (Debug/Release) as this project
+rem And that the CUDA project has been built before this project.
 rem 
-rem If you don't have a clean checkout, it is recommended to purge CUDAKernels/bin directory before rebuilding 
-rem CUDAKernels (for the purpose of creating fresh installation package). This bin directory tends to accumulate
+rem If you don't have a clean checkout, it is recommended to purge the source bin/ptx directory before rebuilding 
+rem CUDA kernels (for the purpose of creating fresh installation package). This bin directory tends to accumulate
 rem garbage over time and everything present in the source 'ptx' subdirectory will be packed to ptx.zip
 
 rem Change current dir to the parent of ptx dir to get the right directory structure inside the zip file
