ba2b40cee15b4e6e48871d3bf4cf828920840d51
Josef Strunc <josef.strunc@keenswh.com>
2015-07-02 12:12:33 +0200

    Fix and clean of post_build commands
    Mainly the nupic_core_solo.dll is properly copied on BasicNodes build.

--- Sources/Modules/BasicNodes/Module/post_build.cmd
+++ Sources/Modules/BasicNodes/Module/post_build.cmd
@@ -1,4 +1,5 @@
-rem $(SolutionDir) = %1, $(ProjectDir) = %2, $(OutDir) = %3 $(Configuration) = %4
+rem $(SolutionDir) = %1, $(ProjectDir) = %2, $(OutDir) = %3, $(Configuration) = %4
 
-mkdir %2..\..\..\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
-xcopy /y /s %2%3*.* %2..\..\..\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
+mkdir %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
+xcopy /y /s %2%3*.* %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
+xcopy /y %1..\BinaryLibs\GoodAiPlatformLibs\Nupic\nupic_core_solo.dll %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
--- Sources/Platform/BrainSimulator/post_build.cmd
+++ Sources/Platform/BrainSimulator/post_build.cmd
@@ -1,10 +1,4 @@
 rem $(SolutionDir) = %1, $(ProjectDir) = %2, $(OutDir) = %3
 mkdir %2%3modules
 
-rem mkdir %2%3modules\MNIST
-rem xcopy /y %2%3MNIST.* %2%3modules\MNIST 
-rem mkdir %2%3modules\XmlFeedForwardNet
-rem xcopy /y %2%3XmlFeedForwardNet.* %2%3modules\XmlFeedForwardNet
-rem xcopy /s /y %1CustomModels\bin\doc %2%3doc
-rem xcopy /s /y %1BrainSimulator\bin\doc %2%3doc
-rem xcopy /y %1..\Libraries\nupic.core\NupicCoreSoloWrapper\x64\Release\NupicCoreSoloWrapper\nupic_core_solo.dll %2%3
+rem just creating the modules directory
