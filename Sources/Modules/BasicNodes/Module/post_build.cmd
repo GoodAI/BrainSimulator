694e6f526f777f8b5049d1e4c28c497c06218965
Josef Strunc <josef.strunc@keenswh.com>
2015-07-03 14:47:18 +0200

    NupicNode removed
    This node will be distributed through separate repository as a
    standalone module under the GPL v3 license.

--- Sources/Modules/BasicNodes/Module/post_build.cmd
+++ Sources/Modules/BasicNodes/Module/post_build.cmd
@@ -2,4 +2,3 @@ rem $(SolutionDir) = %1, $(ProjectDir) = %2, $(OutDir) = %3, $(Configuration) = 
 
 mkdir %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
 xcopy /y /s %2%3*.* %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
-xcopy /y %1..\BinaryLibs\GoodAiPlatformLibs\Nupic\nupic_core_solo.dll %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
