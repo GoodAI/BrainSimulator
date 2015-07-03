a7c992a9b49167b645fca7f79ade00ae3bfaa637
DusanFedorcak <dusan.fedorcak@keenswh.com>
2015-07-03 17:11:31 +0200

    GUI postbuild event to copy license folder

--- Sources/Platform/BrainSimulator/post_build.cmd
+++ Sources/Platform/BrainSimulator/post_build.cmd
@@ -1,4 +1,7 @@
 rem $(SolutionDir) = %1, $(ProjectDir) = %2, $(OutDir) = %3
+
+rem Creating the modules directory...
 mkdir %2%3modules
 
-rem just creating the modules directory
+mkdir %2%3Licenses
+xcopy /y /s %2..\..\..\BinaryLibs\GoodAiPlatformLibs\Licenses\*.* %2%3\Licenses
