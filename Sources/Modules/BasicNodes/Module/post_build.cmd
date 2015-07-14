d3c4f7a33fee127b24aa5dd3431c48481bf217ae
DusanFedorcak <dusan.fedorcak@keenswh.com>
2015-07-14 13:16:37 +0200

    fix of paths inside basicNodes post_build.cmd

--- Sources/Modules/BasicNodes/Module/post_build.cmd
+++ Sources/Modules/BasicNodes/Module/post_build.cmd
@@ -1,4 +1,4 @@
 rem $(SolutionDir) = %1, $(ProjectDir) = %2, $(OutDir) = %3, $(Configuration) = %4
 
-mkdir %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
-xcopy /y /s %2%3*.* %1\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
+mkdir %2..\..\..\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
+xcopy /y /s %2%3*.* %2..\..\..\Platform\BrainSimulator\bin\%4\modules\GoodAI.BasicNodes
