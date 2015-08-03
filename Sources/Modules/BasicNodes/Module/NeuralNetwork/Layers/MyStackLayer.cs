20f46ea88ace9ef27e3223313f9646688ec594bc
MartinHyben <martin.hyben@keenswh.com>
2015-08-03 10:33:20 +0200

    MyStackLayer

--- Sources/Modules/BasicNodes/Module/NeuralNetwork/Layers/MyStackLayer.cs
+++ Sources/Modules/BasicNodes/Module/NeuralNetwork/Layers/MyStackLayer.cs
@@ -15,66 +15,51 @@ using YAXLib;
 
 namespace GoodAI.Modules.NeuralNetwork.Layers
 {
-    public class MyStackLayer : MyAbstractWeightLayer, IMyCustomTaskFactory
+    public class MyStackLayer : MyAbstractLayer, IMyCustomTaskFactory
     {
+        [MyInputBlock(1)]
+        public MyMemoryBlock<float> Input1
+        {
+            get { return GetInput(1); }
+        }
+
+        // pointer to both layers
+        new internal MyAbstractLayer[] PreviousLayer { get; set; }
+
         public override ConnectionType Connection
         {
             get { return ConnectionType.FULLY_CONNECTED; }
         }
         
-        //public MyStackInputsTask StackInputs { get; private set; }
-
         public MyStackLayer()
         {
-            InputBranches = 2;
-            this.ActivationFunction = ActivationFunctionType.NO_ACTIVATION;
+            
         }
-
 
         public override void UpdateMemoryBlocks()
         {
             base.UpdateMemoryBlocks();
 
-            if (Neurons > 0)
-            {
-                if (Input != null)
-                {
-                    // parameter allocations
-                    Weights.Count = Neurons * Input.Count;
-                    Bias.Count = Neurons;
-
-                    // SGD allocations
-                    Delta.Count = Neurons;
-                    PreviousWeightDelta.Count = Neurons * Input.Count; // momentum method
-                    PreviousBiasDelta.Count = Neurons; // momentum method
-
-                    // RMSProp allocations
-                    MeanSquareWeight.Count = Weights.Count;
-                    MeanSquareBias.Count = Bias.Count;
-                }
-            }
-
-            // StackInputs operation
             int totalOutputs = 0;
             Output.ColumnHint = 1;
 
+            PreviousLayer = new MyAbstractLayer[InputBranches];
             for (int i = 0; i < InputBranches; i++)
             {
-                MyMemoryBlock<float> ai = GetInput(i);
+                MyMemoryBlock<float> input = GetInput(i);
 
-                if (ai == null)
+                if (input == null)
                     continue;
 
-                totalOutputs += ai.Count;
+                PreviousLayer[i] = input.Owner as MyAbstractLayer;
+                totalOutputs += input.Count;
 
-                if (Output.ColumnHint == 1 && ai.ColumnHint > 1)
-                {
-                    Output.ColumnHint = ai.ColumnHint;
-                }
+                if (Output.ColumnHint == 1 && input.ColumnHint > 1)
+                    Output.ColumnHint = input.ColumnHint;
             }
 
+            // StackInputs operation
             Output.Count = totalOutputs;
-            Neurons = totalOutputs;
         }
 
         public override string Description
@@ -89,70 +74,44 @@ namespace GoodAI.Modules.NeuralNetwork.Layers
         {
             ForwardTask = new MyStackForwardTask();
             DeltaBackTask = new MyStackBackDeltaTask();
-            //StackInputs = new MyStackInputsTask();
         }
 
         [Description("DeltaBackTask"), MyTaskInfo(OneShot = false)]
-        public class MyStackBackDeltaTask : MyTask<MyAbstractLayer>
+        public class MyStackBackDeltaTask : MyTask<MyStackLayer>
         {
             public MyStackBackDeltaTask() { } //parameterless constructor
 
-            public override void Init(int nGPU)
-            {
-
-            }
+            public override void Init(int nGPU) { }
 
             public override void Execute() //Task execution
             {
-                // pointer to both layers
-                MyAbstractLayer previousLayer = Owner.PreviousLayer;
-                MyAbstractLayer nextLayer = Owner.NextLayer;
+                // propagate delta
+                Owner.Delta.CopyFromMemoryBlock(Owner.NextLayer.Delta, 0, 0, Owner.NextLayer.Delta.Count);
 
-                if (!(Owner.PreviousLayer is MyAbstractLayer) || !(Owner.PreviousLayer is MyAbstractLayer))
-                    return;
-
-                if (previousLayer != null && nextLayer != null)
-                {
-                    // propagate delta
-                    //nextLayer.Delta.CopyToMemoryBlock(previousLayer.Delta, 0, 0, nextLayer.Delta.Count);
-                    Owner.Delta.CopyFromMemoryBlock(nextLayer.Delta, 0, 0, nextLayer.Delta.Count);
-                    Owner.Delta.CopyFromMemoryBlock(previousLayer.Delta, 0, 0, previousLayer.Delta.Count);
-
-                    previousLayer.Delta.SafeCopyToHost();
-                    Owner.Delta.SafeCopyToHost();
-                    nextLayer.Delta.SafeCopyToHost();
-                }
+                for (int i = 0; i < Owner.InputBranches; i++)
+                    Owner.PreviousLayer[i].Delta.CopyFromMemoryBlock(Owner.Delta, 0, 0, Owner.Delta.Count);
             }
         }
 
         [Description("ForwardTask"), MyTaskInfo(OneShot = false)]
-        public class MyStackForwardTask : MyTask<MyAbstractLayer>
+        public class MyStackForwardTask : MyTask<MyStackLayer>
         {
             public MyStackForwardTask() { } //parameterless constructor
 
-            private MyCudaKernel m_forwardKernel; // kernel
-            MyMemoryBlock<float> in0, in1, out0;
-
-            public override void Init(int nGPU)
-            {
-                in0 = Owner.GetInput(0);
-                in1 = Owner.GetInput(1);
-                out0 = Owner.GetOutput(0);
-                m_forwardKernel = MyKernelFactory.Instance.Kernel(nGPU, @"NeuralNetwork\Layer\FeedForwardKernels", "OneToOneForwardKernel");
-            }
+            public override void Init(int nGPU) { }
 
             public override void Execute() //Task execution
             {
                 int totalOutputs = 0;
                 for (int i = 0; i < Owner.InputBranches; i++)
                 {
-                    MyMemoryBlock<float> ai = Owner.GetInput(i);
+                    MyMemoryBlock<float> input = Owner.GetInput(i);
 
-                    if (ai == null)
+                    if (input == null)
                         continue;
 
-                    ai.CopyToMemoryBlock(Owner.Output, 0, totalOutputs, ai.Count);
-                    totalOutputs += ai.Count;
+                    input.CopyToMemoryBlock(Owner.Output, 0, totalOutputs, input.Count);
+                    totalOutputs += input.Count;
                 }
                 Owner.Output.SafeCopyToHost();
             }
