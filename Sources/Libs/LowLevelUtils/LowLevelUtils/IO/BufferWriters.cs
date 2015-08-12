0d1f1c405b760a3e6aca17f768d123dba046924e
PremekPaska <premysl.paska@keenswh.com>
2015-08-12 19:01:23 +0200

    Delete the old BufferWriter.

--- Sources/Libs/LowLevelUtils/LowLevelUtils/IO/BufferWriters.cs
+++ Sources/Libs/LowLevelUtils/LowLevelUtils/IO/BufferWriters.cs
@@ -6,32 +6,6 @@ using System.Threading.Tasks;
 
 namespace GoodAI.LowLevelUtils.IO
 {
-    public abstract class AbstractBufferWriter : AbstractBufferReaderWriter
-    {
-        protected abstract void PutDoubleUnchecked(double d);
-
-        public void PutDouble(double d)
-        {
-            CheckBufferSpace(1);
-            PutDoubleUnchecked(d);
-        }
-
-        protected abstract void PutFloatUnchecked(float f);
-
-        public void PutFloat(float f)
-        {
-            CheckBufferSpace(1);
-            PutFloatUnchecked(f);
-        }
-
-        public abstract void PutInt(int i);
-
-        public void PutBool(bool b)
-        {
-            PutInt(Convert.ToInt32(b));
-        }
-    }
-
     public class RawFloatBufferWriter : AbstractBufferReaderWriter, IRawBufferWriter
     {
         protected override void SetTypedBuffer(Array array)
@@ -112,47 +86,6 @@ namespace GoodAI.LowLevelUtils.IO
         }
 
         #endregion
-    }
-
-    public class FloatBufferWriter : AbstractBufferWriter
-    {
-        private float[] buffer;
-
-        protected override void SetTypedBuffer(Array array)
-        {
-            buffer = (float[])array;
-        }
-
-        /// <summary>
-        /// The user must set Buffer before use.
-        /// </summary>
-        public FloatBufferWriter()
-        { }
-
-        public FloatBufferWriter(float[] externalBuffer)
-        {
-            if (externalBuffer == null)
-                throw new ArgumentNullException();
-
-            Buffer = externalBuffer;
-        }
-
-        protected override void PutDoubleUnchecked(double d)
-        {
-            buffer[index++] = (float)d;
-        }
-
-        protected override void PutFloatUnchecked(float f)
-        {
-            buffer[index++] = f;
-        }
-
-        public override void PutInt(int i)
-        {
-            CheckBufferSpace(1);
-
-            buffer[index++] = (float)i;
-        }
     }
 
     public static class BufferWriterFactory
