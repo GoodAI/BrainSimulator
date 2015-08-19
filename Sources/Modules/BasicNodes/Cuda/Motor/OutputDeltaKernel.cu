89241fb4ab8b4f071de0649710d704cd877e35ad
Karol Kuna <karol.kuna@keenswh.com>
2015-08-19 09:48:08 +0200

    recurrent network srn fix

--- Sources/Modules/BasicNodes/Cuda/Motor/OutputDeltaKernel.cu
+++ Sources/Modules/BasicNodes/Cuda/Motor/OutputDeltaKernel.cu
@@ -20,14 +20,14 @@ extern "C"
 
 	
 	//kernel code
-	__global__ void OutputDeltaKernel(float *activation, float *target, float *outputDelta)
+	__global__ void OutputDeltaKernel(float *outputDeltas, float *target, float *outputActivations, float *outputActivationDerivatives)
 	{
-		int id = blockDim.x*blockIdx.y*gridDim.x	//rows preceeding current row in grid
+		int unitId = blockDim.x*blockIdx.y*gridDim.x	//rows preceeding current row in grid
 			+ blockDim.x*blockIdx.x				//blocks preceeding current block
 			+ threadIdx.x;
-		if (id < D_OUTPUT_UNITS)
+		if (unitId < D_OUTPUT_UNITS)
 		{
-			outputDelta[id] = target[id] - activation[1 + D_INPUT_UNITS + D_HIDDEN_UNITS + id];
+			outputDeltas[unitId] = (target[unitId] - outputActivations[unitId]) * outputActivationDerivatives[unitId];
 		}
 	}
 }
