8611c50a03a802496a8e3141ce99919761ca362d
Michal Vlasák <michal.vlasak@keenswh.com>
2015-09-16 10:49:40 +0200

    Alpha implementation of VectorOps

---
+++ Sources/Modules/BasicNodes/Module/Transforms/MyVectorOpsNode.cs
@@ -0,0 +1,135 @@
+﻿using GoodAI.Core.Memory;
+using GoodAI.Core.Nodes;
+using GoodAI.Core.Task;
+using GoodAI.Core.Utils;
+using GoodAI.Modules.Matrix;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YAXLib;
+
+namespace GoodAI.Modules.Transforms
+{
+    public class MyVectorOpsNode : MyWorkingNode
+    {
+        [MyInputBlock]
+        public MyMemoryBlock<float> InputA
+        {
+            get { return GetInput(0); }
+        }
+
+        [MyInputBlock]
+        public MyMemoryBlock<float> InputB
+        {
+            get { return GetInput(1); }
+        }
+
+        [MyOutputBlock]
+        public MyMemoryBlock<float> Output
+        {
+            get { return GetOutput(0); }
+            set { SetOutput(0, value); }
+        }
+
+        public MyMemoryBlock<float> Transformation { get; private set; }
+        public MyMemoryBlock<float> Temp { get; private set; }
+        public MyMemoryBlock<float> TempVector { get; private set; }
+
+        [MyTaskGroup("VectorOp")]
+        public MyRotateTask RotateTask { get; private set; }
+        [MyTaskGroup("VectorOp")]
+        public MyAngleTask AngleTask { get; private set; }
+
+        public override void UpdateMemoryBlocks()
+        {
+            Output.Count = InputA != null ? InputA.Count : 1;
+            Output.ColumnHint = InputA != null ? InputA.ColumnHint : 1;
+
+            Temp.ColumnHint = Transformation.ColumnHint = Output.Count;
+            Temp.Count = Transformation.Count = Temp.ColumnHint * Temp.ColumnHint;
+
+            TempVector.Count = Output.Count;
+            TempVector.ColumnHint = Output.ColumnHint;
+        }
+
+        [Description("Rotate vector in 2D")]
+        public class MyRotateTask : MyTask<MyVectorOpsNode>
+        {
+            [MyBrowsable, Category("Params")]
+            [YAXSerializableField(DefaultValue = 90)]
+            public int Degrees { get; set; }
+
+            private MyMatrixAutoOps mat_operation;
+
+            public override void Init(int nGPU)
+            {
+                mat_operation = new MyMatrixAutoOps(Owner, Matrix.MatOperation.Multiplication, Owner.Temp);
+            }
+
+            public override void Execute()
+            {
+                double rads = Degrees * Math.PI / 180;
+                float[] transform = new float[] { (float)Math.Cos(rads), -(float)Math.Sin(rads), (float)Math.Sin(rads), (float)Math.Cos(rads) };
+                Array.Copy(transform, Owner.Transformation.Host, transform.Length);
+                Owner.Transformation.SafeCopyToDevice();
+                mat_operation.Run(Matrix.MatOperation.Multiplication, Owner.Transformation, Owner.InputA, Owner.Output);
+            }
+        }
+
+        [Description("Angle between 2 vectors")]
+        public class MyAngleTask : MyTask<MyVectorOpsNode>
+        {
+            [MyBrowsable, Category("Params")]
+            [YAXSerializableField(DefaultValue = false)]
+            public bool Directed { get; set; }
+
+            private MyMatrixAutoOps mat_operation;
+
+            public override void Init(int nGPU)
+            {
+                mat_operation = new MyMatrixAutoOps(Owner, Matrix.MatOperation.Multiplication | Matrix.MatOperation.DotProd, Owner.Temp);
+            }
+
+            public override void Execute()
+            {
+                if (Directed)
+                {
+                    double rads = -Math.PI / 2;
+                    float[] transform = new float[] { (float)Math.Cos(rads), -(float)Math.Sin(rads), (float)Math.Sin(rads), (float)Math.Cos(rads) };
+                    Array.Copy(transform, Owner.Transformation.Host, transform.Length);
+                    Owner.Transformation.SafeCopyToDevice();
+                    mat_operation.Run(Matrix.MatOperation.Multiplication, Owner.Transformation, Owner.InputA, Owner.Output);
+                    Owner.Output.CopyToMemoryBlock(Owner.TempVector, 0, 0, Owner.Output.Count);
+
+                    mat_operation.Run(Matrix.MatOperation.DotProd, Owner.InputA, Owner.InputB, Owner.Output);
+                    Owner.Output.SafeCopyToHost();
+                    float dotProd = Owner.Output.Host[0];
+                    float angle = (float)Math.Acos(dotProd) * 180 / (float)Math.PI;
+
+                    mat_operation.Run(Matrix.MatOperation.DotProd, Owner.TempVector, Owner.InputB, Owner.Output);
+                    Owner.Output.SafeCopyToHost();
+                    float perpDotProd = Owner.Output.Host[0];
+
+                    if (perpDotProd > 0)
+                        angle *= -1;
+                    Owner.Output.Fill(0);
+                    Owner.Output.Host[0] = angle;
+                    Owner.Output.SafeCopyToDevice();
+                }
+                else
+                {
+                    mat_operation.Run(Matrix.MatOperation.DotProd, Owner.InputA, Owner.InputB, Owner.Output);
+                    Owner.Output.SafeCopyToHost();
+                    float dotProd = Owner.Output.Host[0];
+                    float angle = (float)Math.Acos(dotProd) * 180 / (float)Math.PI;
+                    Owner.Output.Fill(0);
+                    Owner.Output.Host[0] = angle;
+                    Owner.Output.SafeCopyToDevice();
+                }
+            }
+        }
+    }
+}
