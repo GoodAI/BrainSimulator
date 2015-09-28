b5d467a6e66fdbcae556ef4ccb9924f674616f70
Karol Kuna <karol.kuna@keenswh.com>
2015-09-28 13:40:25 +0200

    changed warning message

--- Sources/Modules/BasicNodes/Module/NeuralNetwork/Group/MyNeuralNetworkGroup.cs
+++ Sources/Modules/BasicNodes/Module/NeuralNetwork/Group/MyNeuralNetworkGroup.cs
@@ -126,7 +126,7 @@ namespace GoodAI.Modules.NeuralNetwork.Group
                     var layer = n as MyAbstractLayer;
                     if (layer != null && !layer.SupportsBatchLearning())
                     {
-                        validator.AssertError(false, layer, "Does not suport batch-learning but BatchSize=" + BatchSize.ToString() + "!");
+                        validator.AssertError(false, layer, layer.Name + " does not suport batch-learning! Remove the layer or set BatchSize to 1");
                     }
                 }
             }
