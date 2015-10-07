8369e12ed20f02292bfd4bc6b3212eba7211642d
honzakpp <jan.knopp@keenswh.com>
2015-10-07 09:50:10 +0200

    Focuser splitted into Unfocus and Focus, add retina WITHOUT scale so far.   But it is working, check BrainProjectsInternal\Personal\Honza\tests\Test_focuserRetina.brain  for tests

--- Sources/Modules/BasicNodes/Cuda/Transforms/Transform2DKernels.cu
+++ Sources/Modules/BasicNodes/Cuda/Transforms/Transform2DKernels.cu
@@ -301,4 +301,121 @@ extern "C"
 		}
 	}
 
+
+
+
+
+    //------------------------------------------------------------------------------------------------------------------------
+    //                          RETINA STUFF
+    //------------------------------------------------------------------------------------------------------------------------
+
+    __device__ void EstimateParForSubsample(float* subImageDefs, bool safeBounds,
+		int inputWidth, int inputHeight,
+        int2 & subImg, int & diameterPix)
+    {
+    	diameterPix = (int)subImageDefs[2]; // <0,x> in pixels
+
+		subImg.x = (int)(inputWidth * (subImageDefs[0] + 1) * 0.5f);// - diameterPix / 2;
+		subImg.y = (int)(inputHeight * (subImageDefs[1] + 1) * 0.5f);// - diameterPix / 2;
+
+		int maxDiameter = min(inputWidth - 1, inputHeight - 1);
+
+        diameterPix = max(1, diameterPix);
+		diameterPix = min(maxDiameter, diameterPix);
+
+		if (safeBounds) 
+		{
+			subImg.x = max(subImg.x, 1);
+			subImg.y = max(subImg.y, 1);
+			subImg.x = min(subImg.x, inputWidth - diameterPix - 1);
+			subImg.y = min(subImg.y, inputHeight - diameterPix - 1);			
+		}
+    }
+
+
+    __global__ void RetinaTransform_HaveAtLeastOneValueThere (float * subImageDefs, 
+                                                     float* input, int inputWidth, int inputHeight,
+                                                     float* output,int outputDataSize,
+                                                     float* retinaMask, int retinaDataSize, int retinaMaskColHint,
+                                                     float* retinaDataInserted)
+    {
+        int id_retinaPoint = blockDim.x * blockIdx.y * gridDim.x
+				    + blockDim.x * blockIdx.x
+				    + threadIdx.x;
+
+		int2 subImg;
+        int diameterPix;
+        bool  safeBounds = 0;
+
+
+        EstimateParForSubsample( subImageDefs,  safeBounds, inputWidth,  inputHeight,  subImg, diameterPix );
+
+        if (id_retinaPoint<outputDataSize)
+        {
+            output[id_retinaPoint] = 0; // default value
+            int x_mask = retinaMask[id_retinaPoint*retinaMaskColHint];
+            int y_mask = retinaMask[id_retinaPoint*retinaMaskColHint+1];
+
+            int x = subImg.x + x_mask;
+            int y = subImg.y + y_mask;
+            if (x<inputWidth && y<inputHeight && x>=0 && y>=0)
+            {
+                float val = input[x+y*inputWidth];
+                output[id_retinaPoint] = val;
+
+                atomicAdd(output + id_retinaPoint , val);
+                atomicAdd(retinaDataInserted + id_retinaPoint , 1);
+            }
+        }
+    }
+
+    __global__ void RetinaTransform_FillRetinaAtomic (float * subImageDefs, 
+                                                       float* input, int inputWidth, int inputHeight,
+                                                       float* output,int outputDataSize,
+                                                       float* retinaMask, int retinaDataSize, int retinaMaskColHint,
+                                                       float* retinaDataInserted)
+    {
+        int id_pxl = blockDim.x * blockIdx.y * gridDim.x
+				    + blockDim.x * blockIdx.x
+				    + threadIdx.x;
+
+		int2 subImg;
+        int diameterPix;
+        bool  safeBounds = 0;
+
+        int x = id_pxl % inputWidth;
+        int y = id_pxl/inputWidth;
+
+        EstimateParForSubsample( subImageDefs,  safeBounds, inputWidth,  inputHeight,  subImg, diameterPix );
+
+        if (id_pxl<inputWidth*inputHeight)
+        {
+            float minDist = 999999.9; // ??>? should be written bette
+            int minIdx = 1;
+            for (int id_retinaPoint=0 ; id_retinaPoint<retinaDataSize ; id_retinaPoint++)
+            {
+                int x_mask = retinaMask[id_retinaPoint*retinaMaskColHint];
+                int y_mask = retinaMask[id_retinaPoint*retinaMaskColHint+1];
+
+                x_mask += subImg.x;
+                y_mask += subImg.y;
+
+                float dist = (x-x_mask)*(x-x_mask) + (y-y_mask)*(y-y_mask);
+
+                if (dist<minDist)
+                {
+                    minDist = dist;
+                    minIdx  = id_retinaPoint;
+                }
+            }
+            atomicAdd(output + minIdx , input[id_pxl]);
+            atomicAdd(retinaDataInserted + minIdx , 1);
+        }
+    }
+
+
+
+    
+
 }
+
---
+++ Sources/Modules/BasicNodes/Module/Retina/MyUnFocuser.cs
@@ -0,0 +1,129 @@
+﻿using GoodAI.Core;
+using GoodAI.Core.Memory;
+using GoodAI.Core.Task;
+using GoodAI.Core.Utils;
+using GoodAI.Modules.Transforms;
+using System.ComponentModel;
+using System;
+using YAXLib;
+
+namespace GoodAI.Modules.Retina
+{
+    /// <author>GoodAI</author>
+    /// <meta>df/jk-retina</meta>
+    ///<status>Working</status>
+    ///<summary>Crops and resizes input image according to pupil control input.
+    ///Pupil control input must contain position and size of focused area.</summary>
+    ///<description>
+    ///<ul>
+    ///  <li>given a lcoation and image it returns patch there</li>
+    ///  <li>given a patch and lcoationj it plots patch there</li>
+    ///  <li>supports multiple patches at once</li>
+    ///  <li>optional retina like format</li>
+    /// </ul>
+    /// </description>
+    
+    public class MyUnfocuser : MyAbstractFocuser
+    {
+      
+
+        public override void UpdateMemoryBlocks()
+        {
+            base.UpdateMemoryBlocks();
+            RetinaPtsDefsMask.Count = (Input != null) ? (Input.Count * 2) : 1; // now retina dimension is the input not OutputWidth :-)
+        }
+
+
+
+        [MyTaskInfo(Disabled = true, OneShot = true), Description("InitRetina")]
+        public class MyUnfocuseInitRetinaTask : MyTask<MyAbstractFocuser>
+        {
+            public override void Init(int nGPU)
+            {
+            }
+
+            public override void Execute()
+            {
+                Owner.InitRetinaMasks();
+            }
+        }
+    
+        /// <summary>
+        /// given tha patch and location it fit it inot a image, this is similar to canvas.
+        /// </summary>
+        public class MyUnfocusTask : MyTask<MyUnfocuser>
+        {
+            private MyCudaKernel m_kernel;
+
+            private int inputWidth, inputHeight, outputWidth, outputHeight;
+
+            public override void Init(int nGPU)
+            {
+                inputWidth = Owner.Input.ColumnHint;
+                inputHeight = Owner.Input.Count / Owner.Input.ColumnHint;
+
+                outputWidth = Owner.Output.ColumnHint;
+                outputHeight = Owner.Output.Count / Owner.Output.ColumnHint;
+
+                m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Transforms\Transform2DKernels", "BilinearAddSubImageKernel");
+                m_kernel.SetupExecution(Owner.Output.Count);
+            }
+
+            public override void Execute()
+            {
+                if (Owner.NumberPupilSamples > 1)
+                {
+                    MyLog.WARNING.WriteLine("MyFocuser:MyUnfocusTask deoes not support multiple pupils input!");
+                }
+                else
+                {
+                    Owner.Output.Fill(0);
+                    m_kernel.Run(Owner.Output, Owner.Input, Owner.PupilControl, outputWidth, outputHeight, inputWidth, inputHeight);
+                }
+            }
+        };
+
+
+        /// <summary>
+        /// Given a retina patch and its location, it plots it into a postion.
+        /// </summary>
+        [MyTaskInfo(Disabled = true)]
+        public class MyRetinaUnfocusTask : MyTask<MyUnfocuser>
+        {
+            private MyCudaKernel m_kernel;
+
+            private int outputWidth, outputHeight;
+
+            public override void Init(int nGPU)
+            {
+                outputWidth = Owner.Output.ColumnHint;
+                outputHeight = Owner.Output.Count / Owner.Output.ColumnHint;
+
+                m_kernel = MyKernelFactory.Instance.Kernel(MyKernelFactory.Instance.DevCount - 1, @"Observers\FocuserInputObserver", "RetinaObserver_UnMaskPatchFl");
+                m_kernel.SetupExecution(Owner.Output.Count);
+            }
+
+            public override void Execute()
+            {
+                if (Owner.NumberPupilSamples > 1)
+                {
+                    MyLog.WARNING.WriteLine("MyFocuser:MyRetinaUnfocusTask deoes not support multiple pupils input! (so far)");
+                }
+                else
+                {
+                    if (SimulationStep == 0) // init mask
+                    {
+                        Owner.InitRetinaMasks();
+                    }
+                    m_kernel.Run(Owner.Output, outputWidth, outputHeight, Owner.RetinaPtsDefsMask, Owner.RetinaPtsDefsMask.Count / 2, Owner.Input, Owner.PupilControl);
+                }
+            }
+        };
+
+
+
+        public MyUnfocuseInitRetinaTask DoInitRetina { get; private set; }
+        public MyUnfocusTask DoInverseTransform { get; private set; }
+        public MyRetinaUnfocusTask DoRetinaUnfocus { get; private set; }
+    }
+}
