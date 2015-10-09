71201033d8b2f9ef543942e1793c37ff6794b3d8
Michal Vlasák <michal.vlasak@keenswh.com>
2015-10-09 11:50:06 +0200

    VectorOps tests added

---
+++ Sources/Modules/BasicNodes/Tests/VectorOpsTests.cs
@@ -0,0 +1,31 @@
+﻿using GoodAI.BasicNodes.Transforms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BasicNodesTests
+{
+    public class VectorOpsTests
+    {
+        public const int precision = 3;
+
+        [Fact]
+        public void DegreeToRadian()
+        {
+            Assert.Equal(0, VectorOps.DegreeToRadian(0));
+            Assert.Equal(Math.PI, VectorOps.DegreeToRadian(180), precision);
+            Assert.Equal(2 * Math.PI, VectorOps.DegreeToRadian(360), precision);
+        }
+
+        [Fact]
+        public void RadianToDegree()
+        {
+            Assert.Equal(0, VectorOps.RadianToDegree(0));
+            Assert.Equal(180 / Math.PI, VectorOps.RadianToDegree(1), precision);
+            Assert.Equal(180, VectorOps.RadianToDegree((float)Math.PI));
+        }
+    }
+}
