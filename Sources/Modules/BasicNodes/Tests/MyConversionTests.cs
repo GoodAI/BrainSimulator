23ff9204fcdc9ccf79f2cb4ac65f053b593c7c3f
Michal Vlasák <michal.vlasak@keenswh.com>
2015-10-12 16:08:46 +0200

    RandomNode conversion test added

---
+++ Sources/Modules/BasicNodes/Tests/MyConversionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BasicNodesTests
+{
+    public class MyConversionTests
+    {
+        [Fact]
+        public void Convert6To7()
+        {
+            string xml = BasicNodesTests.Properties.Resources.Convert6To7XML;
+            string expected = BasicNodesTests.Properties.Resources.Convert6To7XMLResult;
+            string result = GoodAI.Modules.Versioning.MyConversion.Convert6To7(xml);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
