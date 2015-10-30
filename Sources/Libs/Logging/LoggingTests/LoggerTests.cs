1f05848dea9dbce3760344d66a43b6e8aa78e273
Honza Sinkora <jan.sinkora@keenswh.com>
2015-10-30 13:39:35 +0100

    Added a simple test for logging

--- Sources/Libs/Logging/LoggingTests/LoggerTests.cs
+++ Sources/Libs/Logging/LoggingTests/LoggerTests.cs
@@ -16,7 +16,7 @@ namespace LoggingTests
 {
     public class LoggerTests
     {
-        class TestSink : ILogEventSink
+        private class TestSink : ILogEventSink
         {
             public List<LogEvent> Events { get; private set; }
 
@@ -31,20 +31,17 @@ namespace LoggingTests
             }
         }
 
-        //[Fact(Skip="Manual test")]
         [Fact]
         public void ManualTest()
         {
             var infoSink = new TestSink();
             var debugSink = new TestSink();
-            var writer = new StringWriter();
 
             Logger.Setup(configuration =>
             {
                 configuration
                     .WriteTo.Sink(debugSink)
-                    .WriteTo.Sink(infoSink, restrictedToMinimumLevel: LogEventLevel.Information)
-                    .WriteTo.TextWriter(writer, outputTemplate: Logger.DefaultOutputTemplate);
+                    .WriteTo.Sink(infoSink, restrictedToMinimumLevel: LogEventLevel.Information);
 
                 return configuration;
             });
@@ -52,76 +49,17 @@ namespace LoggingTests
             Logger.Instance.Debug("debug 1");
             Logger.Instance.Debug("debug 2: {Message}", "foo");
             Logger.Instance.Information("info 1");
-            Logger.Instance.Information("info 2: {AnotherMessage}", "bar");
+            Logger.Instance.Information("info 2: {AnotherMessage:l}", "bar");
 
             var logger = Logger.Instance.ForContext<LoggerTests>();
             logger.Debug("debug 3");
             logger.Information("info 3");
 
-            try
-            {
-                ThrowSimpleException();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex, "{Component} failed", "Something");
-            }
+            Assert.Equal(6, debugSink.Events.Count);
+            Assert.Equal(3, infoSink.Events.Count);
 
-            try
-            {
-                ThrowAggregateException();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex, "Multiple failures in {Component}", "SomethingElse");
-            }
-
-            try
-            {
-                ThrowInnerException();
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex, "{Component} failed because of an inner exception", "SomethingWeird");
-            }
-
-            var debugResult = debugSink.ToString();
-            var infoResult = infoSink.ToString();
-            var writerResult = writer.ToString();
-        }
-
-        private void ThrowSimpleException()
-        {
-            throw new Exception("aaa");
-        }
-
-        private void ThrowAggregateException()
-        {
-            var results = new List<Exception>();
-            for (int i = 0; i < 3; i++)
-            {
-                try
-                {
-                    ThrowSimpleException();
-                }
-                catch (Exception ex)
-                {
-                    results.Add(ex);
-                }
-            }
-            throw new AggregateException("ccc", results);
-        }
-
-        private void ThrowInnerException()
-        {
-            try
-            {
-                ThrowSimpleException();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("bbb", ex);
-            }
+            Assert.Contains("debug 2: \"foo\"", debugSink.Events.Select(e => e.RenderMessage()));
+            Assert.Contains("info 2: bar", debugSink.Events.Select(e => e.RenderMessage()));
         }
     }
 }
