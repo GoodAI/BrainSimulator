b357b321d7e9f80e51f821b7797104a35ba7b6eb
Vegard Lillevoll <vegard.lillevoll@keenswh.com>
2015-11-11 16:54:24 +0100

    InternalNodes should now be copied correctly if the directory exists

--- Sources/Platform/CoreRunner/post_build.cmd
+++ Sources/Platform/CoreRunner/post_build.cmd
@@ -7,8 +7,10 @@ rem Copying modules...
 mkdir %2%3modules\GoodAI.BasicNodes
 xcopy /y /s %2..\..\Modules\BasicNodes\Module\bin\%4\*.* %2%3\modules\GoodAI.BasicNodes
 
-rem mkdir %2%3modules\GoodAI.InternalNodes
-rem xcopy /y /s %2..\..\Modules\InternalNodes\Module\bin\%4\*.* %2%3\modules\GoodAI.InternalNodes
+if not exist %2..\..\Modules\InternalNodes goto SKIPINTERNALNODES
+mkdir %2%3modules\GoodAI.InternalNodes
+xcopy /y /s %2..\..\Modules\InternalNodes\Module\bin\%4\*.* %2%3\modules\GoodAI.InternalNodes
+:SKIPINTERNALNODES
 
 mkdir %2%3modules\GoodAI.MNIST
 xcopy /y /s %2..\..\Modules\MNIST\Module\bin\%4\*.* %2%3\modules\GoodAI.MNIST
