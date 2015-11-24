9f72e174eedd3450034ec3e336eabb94b57a1b02
Honza Sinkora <jan.sinkora@keenswh.com>
2015-11-24 15:15:08 +0100

    Adding nodes usable for testing

---
+++ Sources/Modules/TestingNodes/Module/DynamicBlockNode.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.Core.Memory;
+using GoodAI.Core.Nodes;
+using GoodAI.Core.Task;
+using GoodAI.Core.Utils;
+
+namespace GoodAI.Modules.TestingNodes.DynamicBlocks
+{
+    /// <summary>
+    /// A node that reallocates the output block in each step to a random size and fills it with random data.
+    /// </summary>
+    public class DynamicOutputNode : MyWorkingNode
+    {
+        public Random Random { get; private set; }
+
+        public DynamicOutputNode()
+        {
+            Random = new Random();
+        }
+
+        [MyOutputBlock(0), DynamicBlock]
+        public MyMemoryBlock<float> Output
+        {
+            get { return GetOutput(0); }
+            set { SetOutput(0, value); }
+        }
+
+        public DynamicOutputTask Task { get; private set; }
+
+        public override void UpdateMemoryBlocks()
+        {
+            Output.Count = 50;
+        }
+
+        public int GetNewBufferSize()
+        {
+            return Random.Next(99) + 1;
+        }
+    }
+
+    /// <summary>
+    /// See DynamicOutputNode.
+    /// </summary>
+    [Description("Dynamic block task")]
+    public class DynamicOutputTask : MyTask<DynamicOutputNode>
+    {
+        public override void Init(int nGPU)
+        {
+        }
+
+        public override void Execute()
+        {
+            if (Owner.Output.Reallocate(Owner.GetNewBufferSize()))
+            {
+                MyLog.INFO.WriteLine("Reallocated the output block");
+
+                for (int i = 0; i < Owner.Output.Count; i++)
+                    Owner.Output.Host[i] = Owner.Random.Next(100);
+
+                MyLog.INFO.WriteLine("Source output count: " + Owner.Output.Count);
+                MyLog.INFO.WriteLine("Source output sum: " + Owner.Output.Host.Sum());
+
+                Owner.Output.SafeCopyToDevice();
+            }
+            else
+            {
+                MyLog.WARNING.WriteLine("Failed to reallocate");
+            }
+        }
+    }
+
+    /// <summary>
+    /// A node that takes dynamic input, reallocates the output to match the input, copies the data
+    /// using the GPU and prints out control sums.
+    /// </summary>
+    public class DynamicInputNode : MyWorkingNode
+    {
+        [MyInputBlock(0), DynamicBlock]
+        public MyMemoryBlock<float> Input
+        {
+            get { return GetInput(0); }
+        }
+
+        [MyOutputBlock(0), DynamicBlock]
+        public MyMemoryBlock<float> Output
+        {
+            get { return GetOutput(0); }
+            set { SetOutput(0, value); }
+        }
+
+        public DynamicInputTask Task { get; private set; }
+
+        public override void UpdateMemoryBlocks()
+        {
+            Output.Count = Input.Count;
+        }
+    }
+
+    /// <summary>
+    /// See DynamicInputNode.
+    /// </summary>
+    [Description("Dynamic block task")]
+    public class DynamicInputTask : MyTask<DynamicInputNode>
+    {
+        public override void Init(int nGPU)
+        {
+        }
+
+        public override void Execute()
+        {
+            Owner.Output.Reallocate(Owner.Input.Count);
+
+            Owner.Output.GetDevice(Owner.GPU).CopyToDevice(Owner.Input.GetDevicePtr(Owner.GPU));
+
+            Owner.Input.SafeCopyToHost();
+            MyLog.INFO.WriteLine("Destination input count: " + Owner.Input.Count);
+            MyLog.INFO.WriteLine("Destination input sum: " + Owner.Input.Host.Sum());
+
+            Owner.Output.SafeCopyToHost();
+            MyLog.INFO.WriteLine("Destination output count: " + Owner.Output.Count);
+            MyLog.INFO.WriteLine("Destination output sum: " + Owner.Output.Host.Sum());
+        }
+    }
+}
---
+++ Sources/Modules/TestingNodes/Module/Versioning/MyConversion.cs
@@ -0,0 +1,13 @@
+﻿using GoodAI.Core.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace GoodAI.Modules.Versioning
+{
+    public class MyConversion : MyBaseConversion
+    {
+        public override int CurrentVersion { get { return 1; } }
+    }
+}
