36a07f1d98ad944c03c640b7598a5775ec7153f4
Martin Poliak <martin.poliak@keenswh.com>
2015-12-21 12:48:51 +0100

    Stretching of texture masks.

--- Sources/Modules/BasicNodes/Cuda/Drawing/RgbaDrawing.cu
+++ Sources/Modules/BasicNodes/Cuda/Drawing/RgbaDrawing.cu
@@ -167,7 +167,7 @@ extern "C"
 	}
 
 	/*
-	Draws a texture into a 3-component target. RGBA. Checks bounds.
+	Draws a texture into a 3-component target. RGBA. Checks bounds. Stretches the texture.
 	*/
 	__global__ void DrawRgbaTextureKernelNearestNeighbor(float *target, int targetWidth, int targetHeight, int inputX, int inputY,
 		float *texture, int textureWidth, int textureHeight, int objectWidth, int objectHeight)
@@ -219,6 +219,81 @@ extern "C"
 				int aIndex = textureId + 3 * texturePixels; // the A component of the texture
 				float a = texture[aIndex];
 				target[tIndex] = target[tIndex] * (1.0f - a) + a * textureValue;
+			}
+		}
+	}
+
+	/*
+	Same as DrawRgbaTextureKernelNearestNeighbor, but texture = mask and texture's pixel values are replaced by a single color
+	*/
+	__global__ void DrawMaskedColorKernelNearestNeighbor(float *target, int targetWidth, int targetHeight, int inputX, int inputY,
+		float *texture, int textureWidth, int textureHeight, int objectWidth, int objectHeight, 
+		float r, float g, float b ) // texture = mask
+	{
+		int id = blockDim.x * blockIdx.y * gridDim.x
+			+ blockDim.x * blockIdx.x
+			+ threadIdx.x;
+
+		int targetPixels = targetWidth * targetHeight;
+
+		int texturePixels = textureWidth * textureHeight;
+
+		int objectPixels = objectWidth * objectHeight;
+
+		int idObjectRgb = id / objectPixels;
+		int idObjectPixel = (id - idObjectRgb * objectPixels); // same as (id % objectPixels), but the kernel runs 10% faster
+		int idObjectY = idObjectPixel / objectWidth;
+		int idObjectX = (idObjectPixel - idObjectY * objectWidth); // same as (id % textureWidth), but the kernel runs another 10% faster
+
+
+		if (idObjectRgb < 3) // 3 channels that we will write to
+		{
+			int targetRgb = idObjectRgb;
+			// the texture is in BGR format, we want RGB
+			switch (idObjectRgb)
+			{
+			case 0: // R
+				targetRgb = 2; // B
+				break;
+			case 2: // B
+				targetRgb = 0; // R
+				break;
+			}
+			// if the object pixel offset by inputX, inputY, lies inside the target
+			if (idObjectX + inputX < targetWidth &&
+				idObjectX + inputX >= 0 &&
+				idObjectY + inputY < targetHeight &&
+				idObjectY + inputY >= 0)
+			{
+				// nearest neighbor texture X,Y:
+				int textureX = textureWidth * idObjectX / objectWidth;
+				int textureY = textureHeight * idObjectY / objectHeight;
+				int textureId = textureY * textureWidth + textureX;
+
+				int rgbIndex = textureId + idObjectRgb * texturePixels;
+
+				int tIndex = targetPixels * targetRgb + targetWidth * (idObjectY + inputY) + (idObjectX + inputX);
+				int aIndex = textureId + 3 * texturePixels; // the A component of the texture
+				float a = texture[aIndex];
+
+				if (a > 0) // mask allows color here
+				{
+					// apply this: target[tIndex] = target[tIndex] * (1.0f - a) + a * color;
+					target[tIndex] = target[tIndex] * (1.0f - a);
+					switch (idObjectRgb)
+					{
+					case 0:
+						target[tIndex] += a*r;
+						break;
+					case 1:
+						target[tIndex] += a*g;
+						break;
+					case 2:
+					default:
+						target[tIndex] += a*b;
+						break;
+					}
+				}
 			}
 		}
 	}
