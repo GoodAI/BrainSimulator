3fe1101a864959dc578b2063ed4a99d7339cc00b
PremekPaska <premysl.paska@keenswh.com>
2016-01-06 15:50:34 +0100

    Add testing "inline" brain test to TestingNodes.

---
+++ Sources/Modules/TestingNodes/Module/BrainUnitNodeSelfTests.cs
@@ -0,0 +1,24 @@
+﻿using GoodAI.Testing.BrainUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodAI.Modules.TestingNodes
+{
+    public sealed class BrainTestCanBeDiscovered : BrainTest
+    {
+        public BrainTestCanBeDiscovered()
+        {
+            BrainFileName = "brain-unit-node-self-test.brain";
+            MaxStepCount = 1;
+        }
+
+        public override void Check(IBrainScan b)
+        {
+            // pass
+        }
+
+    }
+}
