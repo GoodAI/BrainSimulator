913d588113d11ac13bc980456783086c43cb1a0a
Martin Brázdil <martin.brazdil@keenswh.com>
2016-01-25 19:03:53 +0100

    New API almost finished.

--- Sources/Platform/Core/Transforms/MyReduction.cs
+++ Sources/Platform/Core/Transforms/MyReduction.cs
@@ -1,162 +1,201 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using GoodAI.Core;
-using GoodAI.Core.Memory;
+using GoodAI.Core.Nodes;
+using GoodAI.Core.Utils;
 using ManagedCuda;
 using ManagedCuda.BasicTypes;
 using ManagedCuda.VectorTypes;
 using System.Runtime.InteropServices;
+using GoodAI.Core.Memory;
 
 namespace GoodAI.Modules.Transforms
 {
-    public enum ReductionMode
+
+    public class MyParallelKernel<T> where T : struct
     {
-        i_Sum_i, i_MinIdx_2i, i_MaxIdx_2i, i_MinIdxMaxIdx_4i, c_Sum_c,
-        f_Sum_f, f_MinIdx_fi, f_MinIdx_ff, f_MaxIdx_fi, f_MaxIdx_ff, f_MinIdxMaxIdx_fifi, f_MinMax_2f
-    };
+        protected int m_nGPU;
+        protected MyNode m_owner;
+        protected MyCudaKernel m_kernel;
 
-    public enum ProductMode
-    {
-        f_DotProduct_f, i_DotProduct_i, f_Cosine_f, c_ComplexDot_c
-    };
+        protected MyMemoryBlock<float> m_buffer { get; private set; }
 
-    public class MyReductionKernel<T> where T : struct
-    {
-        private MyCudaKernel m_kernel;
-        CUdeviceptr CUdeviceNullptr = new CUdeviceptr(0);
+        protected int m_TSize;
+        protected int m_outTypeSize;
+        protected int m_inTypeSize;
 
-        private int TSize;
-        private int m_outTypeSize;
-        private int m_inTypeSize;
+        protected dim3 m_blockDims;
+        private dim3 m_gridDims;
 
-        public bool segmented;
-        public bool distributed;
         public int timeOffset;
         public int outOffset;
         public int inOffset;
-        public int stride;
-        int nGPU;
 
-        public MyReductionKernel(int nGPU, ReductionMode mode, bool segmented = false, bool distributed = false)
+        public dim3 gridDims
         {
-            switch (mode)
+            get
             {
-                case ReductionMode.i_Sum_i: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI7i_Sum_iiLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 4; break;
-                case ReductionMode.i_MinIdx_2i: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI11i_MinIdx_2iiLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 8; break;
-                case ReductionMode.i_MaxIdx_2i: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI11i_MaxIdx_2iiLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 8; break;
-                case ReductionMode.i_MinIdxMaxIdx_4i: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI17i_MinIdxMaxIdx_4iiLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 16; break;
-                case ReductionMode.f_Sum_f: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI7f_Sum_ffLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 4; break;
-                case ReductionMode.f_MinMax_2f: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI11f_MinMax_2ffLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 8; break;
-                case ReductionMode.f_MinIdx_fi: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI11f_MinIdx_fifLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 8; break;
-                case ReductionMode.f_MaxIdx_fi: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI11f_MaxIdx_fifLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 8; break;
-                case ReductionMode.f_MinIdxMaxIdx_fifi: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI19f_MinIdxMaxIdx_fififLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 16; break;
-                case ReductionMode.f_MinIdx_ff: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI11f_MinIdx_fffLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 8; break;
-                case ReductionMode.f_MaxIdx_ff: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI11f_MaxIdx_fffLj512EEvPvPVKvS1_jjjjb"); m_inTypeSize = 4; m_outTypeSize = 8; break;
-                case ReductionMode.c_Sum_c: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z9ReductionI7c_Sum_c7ComplexLj512EEvPvPVKvS2_jjjjb"); m_inTypeSize = 8; m_outTypeSize = 8; break;
-                default: throw new ArgumentOutOfRangeException("mode", "Unrecognized reduction mode.");
+                if (m_buffer.Count <= gridDims.x * m_outTypeSize)
+                    throw new Exception("Output buffer size is too small (will overflow).");
+                return m_gridDims;
             }
+            set
+            {
+                m_gridDims.x = value.x;
+                if (value.y != 0 || value.z != 0)
+                    MyLog.Writer.WriteLine(MyLogLevel.WARNING, "MyReduction.cs: Attempt to set reduction gridDims with non-zero y and z values.");
+                m_kernel.SetupExecution(m_blockDims, m_gridDims);
+            }
+        }
 
-            TSize = Marshal.SizeOf(typeof(T));
-            if (TSize > m_inTypeSize || TSize > m_outTypeSize || TSize % m_inTypeSize != 0 ||
-                TSize % m_outTypeSize != 0 || m_inTypeSize == 0 || m_outTypeSize == 0)
-                throw new ArgumentOutOfRangeException("mode", "MemoryBlock type can be incompatible with reduction in/out types.");
+        protected static ParallelKernelDescriptor GetDescriptor(Type enumType, string enumString)
+        {
+            MemberInfo[] memInfo = enumType.GetMember(enumString);
+            object[] attributes = memInfo[0].GetCustomAttributes(typeof(ParallelKernelDescriptor), false);
+            if (attributes.Length == 0)
+                throw new Exception("Kernel descriptor is missing for " + enumString + ".");
+            return attributes[0] as ParallelKernelDescriptor;
+        }
 
+        public MyParallelKernel(MyNode owner, int nGPU, ParallelKernelDescriptor descriptor, int bufferSize)
+        {
+            m_owner = owner;
+            m_nGPU = nGPU;
+
+            m_outTypeSize = descriptor.outTypeSize;
+            m_inTypeSize = descriptor.inTypeSize;
+
+            m_TSize = Marshal.SizeOf(typeof(T));
+            if (m_TSize > m_inTypeSize || m_TSize > m_outTypeSize || m_inTypeSize % m_TSize != 0 ||
+                m_outTypeSize % m_TSize != 0 || m_inTypeSize == 0 || m_outTypeSize == 0)
+                MyLog.Writer.WriteLine(MyLogLevel.WARNING, "MyReduction.cs: MemoryBlock type can be incompatible with reduction in/out types.");
+
+            m_kernel = MyKernelFactory.Instance.Kernel(m_nGPU, @"Common\Reduction\Reduction", descriptor.name);
+
+            m_buffer = MyMemoryManager.Instance.CreateMemoryBlock<float>(owner);
+            m_buffer.Count = bufferSize;
+
+            m_blockDims = new dim3(512);
+            m_gridDims = new dim3(1);
+
+            m_kernel.SetupExecution(m_blockDims, m_gridDims);
+            m_kernel.DynamicSharedMemory = 512 * (uint)m_outTypeSize;
+        }
+    }
+
+    public enum ReductionMode
+    {
+        [ParallelKernelDescriptor("_Z9ReductionI7i_Sum_iiLj512EEvPvPVKvS1_jjjjb", 4, 4)]
+        i_Sum_i,
+        [ParallelKernelDescriptor("_Z9ReductionI11i_MinIdx_2iiLj512EEvPvPVKvS1_jjjjb", 4, 8)]
+        i_MinIdx_2i,
+        [ParallelKernelDescriptor("_Z9ReductionI11i_MaxIdx_2iiLj512EEvPvPVKvS1_jjjjb", 4, 8)]
+        i_MaxIdx_2i,
+        [ParallelKernelDescriptor("_Z9ReductionI17i_MinIdxMaxIdx_4iiLj512EEvPvPVKvS1_jjjjb", 4, 16)]
+        i_MinIdxMaxIdx_4i,
+        [ParallelKernelDescriptor("_Z9ReductionI7f_Sum_ffLj512EEvPvPVKvS1_jjjjb", 4, 4)]
+        f_Sum_f,
+        [ParallelKernelDescriptor("_Z9ReductionI11f_MinMax_2ffLj512EEvPvPVKvS1_jjjjb", 4, 8)]
+        f_MinMax_2f,
+        [ParallelKernelDescriptor("_Z9ReductionI11f_MinIdx_fifLj512EEvPvPVKvS1_jjjjb", 4, 8)]
+        f_MinIdx_fi,
+        [ParallelKernelDescriptor("_Z9ReductionI11f_MaxIdx_fifLj512EEvPvPVKvS1_jjjjb", 4, 8)]
+        f_MaxIdx_fi,
+        [ParallelKernelDescriptor("_Z9ReductionI19f_MinIdxMaxIdx_fififLj512EEvPvPVKvS1_jjjjb", 4, 16)]
+        f_MinIdxMaxIdx_fifi,
+        [ParallelKernelDescriptor("_Z9ReductionI11f_MinIdx_fffLj512EEvPvPVKvS1_jjjjb", 4, 8)]
+        f_MinIdx_ff,
+        [ParallelKernelDescriptor("_Z9ReductionI11f_MaxIdx_fffLj512EEvPvPVKvS1_jjjjb", 4, 8)]
+        f_MaxIdx_ff,
+        [ParallelKernelDescriptor("_Z9ReductionI7c_Sum_c7ComplexLj512EEvPvPVKvS2_jjjjb", 8, 8)]
+        c_Sum_c,
+    }
+
+    public sealed class MyReductionKernel<T> : MyParallelKernel<T> where T : struct
+    {
+        public int stride;
+        public bool segmented;
+
+        public MyReductionKernel(MyNode owner, int nGPU, ReductionMode mode, bool segmented = false, int bufferSize = 8192)
+            : base(owner, nGPU, GetDescriptor(typeof(ReductionMode), mode.ToString()), bufferSize)
+        {
             this.stride = 1;
             this.segmented = segmented;
-            this.distributed = distributed;
-            this.nGPU = nGPU;
         }
 
         public void Run(MyMemoryBlock<T> output, MyMemoryBlock<T> input)
         {
-            // rDeviceVar != null ? rDeviceVar.DevicePointer + offset * rDeviceVar.TypeSize : default(CUdeviceptr);
-            CUdeviceptr outputPtr = output.GetDevicePtr(nGPU, outOffset * (m_outTypeSize / TSize));//, timeOffset);
-            CUdeviceptr inputPtr = input.GetDevicePtr(nGPU, inOffset * (m_inTypeSize / TSize));//, timeOffset);
+            CUdeviceptr outputPtr = output.GetDevicePtr(m_nGPU, outOffset * (m_outTypeSize / m_TSize), timeOffset);
+            CUdeviceptr inputPtr = input.GetDevicePtr(m_nGPU, inOffset * (m_inTypeSize / m_TSize), timeOffset);
 
-            dim3 blockDims = new dim3(512);
-            dim3 gridDims = new dim3(1);
-            m_kernel.SetupExecution(blockDims, gridDims);
-            m_kernel.DynamicSharedMemory = 512 * (uint)m_outTypeSize;
-
-            // void* rawOut, volatile const void* rawIn, void* tempBuffer, unsigned int size, unsigned int outOff, unsigned int inOff, unsigned int stride, bool segmented
             m_kernel.Run(outputPtr, inputPtr, 0, input.Count, 0, 0, stride, Convert.ToInt32(segmented));
         }
 
-        public void RunAsync(CudaStream stream, MyMemoryBlock<T> output, MyMemoryBlock<T> input, MyMemoryBlock<float> buffer)
+        public void RunAsync(CudaStream stream, MyMemoryBlock<T> output, MyMemoryBlock<T> input)
         {
-            // rDeviceVar != null ? rDeviceVar.DevicePointer + offset * rDeviceVar.TypeSize : default(CUdeviceptr);
-            CUdeviceptr outputPtr = output.GetDevicePtr(nGPU, outOffset * (m_outTypeSize / TSize), timeOffset);
-            CUdeviceptr inputPtr = output.GetDevicePtr(nGPU, inOffset * (m_inTypeSize / TSize), timeOffset);
+            CUdeviceptr outputPtr = output.GetDevicePtr(m_nGPU, outOffset * (m_outTypeSize / m_TSize), timeOffset);
+            CUdeviceptr inputPtr = input.GetDevicePtr(m_nGPU, inOffset * (m_inTypeSize / m_TSize), timeOffset);
 
-            // void* rawOut, volatile const void* rawIn, void* tempBuffer, unsigned int size, unsigned int outOff, unsigned int inOff, unsigned int stride, bool segmented
-            m_kernel.RunAsync(stream, outputPtr, inputPtr, buffer, input.Count, 0, 0, 1, Convert.ToInt32(segmented));
+            m_kernel.RunAsync(stream, outputPtr, inputPtr, m_buffer, input.Count, 0, 0, stride, Convert.ToInt32(segmented));
         }
-
-        protected void SetupExecution(int numOfParallelUnits) { }
-        protected virtual void SetupExecution(dim3 blockDimensions, dim3 gridDimensions) { }
     }
 
-    public class MyProductKernel<T> where T : struct
+    public enum ProductMode
     {
-        private MyCudaKernel m_kernel;
+        [ParallelKernelDescriptor("_Z10DotProductI7f_Dot_ffLj512EEvPvjPVKvS3_S1_jbb", 4, 4)]
+        f_DotProduct_f,
+        [ParallelKernelDescriptor("_Z10DotProductI7i_Dot_iiLj512EEvPvjPVKvS3_S1_jbb", 4, 4)]
+        i_DotProduct_i,
+        [ParallelKernelDescriptor("_Z10DotProductI10f_Cosine_ffLj512EEvPvjPVKvS3_S1_jbb", 4, 16)]
+        f_Cosine_f,
+        [ParallelKernelDescriptor("_Z10DotProductI14c_ComplexDot_c7ComplexLj512EEvPvjPVKvS4_S2_jbb", 8, 8)]
+        c_ComplexDot_c
+    }
 
-        private int TSize;
-        private int m_outTypeSize;
-        private int m_inTypeSize;
-
+    public class MyProductKernel<T> : MyParallelKernel<T> where T : struct
+    {
         public bool segmented;
         public bool distributed;
-        public int timeOffset;
-        public int outOffset;
-        public int inOffset;
-        public int stride;
 
-        int nGPU;
-
-        public MyProductKernel(int nGPU, ProductMode mode, bool segmented = false, bool distributed = false)
+        public MyProductKernel(MyNode owner, int nGPU, ProductMode mode, bool segmented = false, bool distributed = false, int bufferSize = 8192)
+            : base(owner, nGPU, GetDescriptor(typeof(ProductMode), mode.ToString()), bufferSize)
         {
-            switch (mode)
-            {
-                case ProductMode.i_DotProduct_i: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z10DotProductI7i_Dot_iiLj512EEvPvjPVKvS3_S1_jbb"); m_inTypeSize = 4; m_outTypeSize = 4; break;
-                case ProductMode.f_DotProduct_f: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z10DotProductI7f_Dot_ffLj512EEvPvjPVKvS3_S1_jbb"); m_inTypeSize = 4; m_outTypeSize = 4; break;
-                case ProductMode.f_Cosine_f: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z10DotProductI10f_Cosine_ffLj512EEvPvjPVKvS3_S1_jbb"); m_inTypeSize = 4; m_outTypeSize = 16; break;
-                case ProductMode.c_ComplexDot_c: m_kernel = MyKernelFactory.Instance.Kernel(nGPU, @"Common\Reduction\Reduction", "_Z10DotProductI14c_ComplexDot_c7ComplexLj512EEvPvjPVKvS4_S2_jbb"); m_inTypeSize = 8; m_outTypeSize = 8; break;
-                default: throw new ArgumentOutOfRangeException("mode", "Unrecognized reduction mode.");
-            }
-
-            TSize = Marshal.SizeOf(typeof(T));
-            if (TSize > m_inTypeSize || TSize > m_outTypeSize || TSize % m_inTypeSize != 0 ||
-                TSize % m_outTypeSize != 0 || m_inTypeSize == 0 || m_outTypeSize == 0)
-                throw new ArgumentOutOfRangeException("mode", "MemoryBlock type can be incompatible with reduction in/out types.");
-
             this.segmented = segmented;
             this.distributed = distributed;
-            this.nGPU = nGPU;
         }
 
         public void Run(MyMemoryBlock<T> output, MyMemoryBlock<T> input1, MyMemoryBlock<T> input2)
         {
-            CUdeviceptr outputPtr = output.GetDevicePtr(nGPU, outOffset * (m_outTypeSize / TSize), timeOffset);
+            CUdeviceptr outputPtr = output.GetDevicePtr(m_nGPU, outOffset * (m_outTypeSize / m_TSize), timeOffset);
 
-            //void* rawOut, unsigned int outOff, volatile const void* rawIn1, volatile const void* rawIn2, void* tempBuffer, unsigned int size, bool segmented, bool distributed
-            m_kernel.Run(outputPtr, 0, input1, input2, 0, input2.Count, Convert.ToInt32(segmented), Convert.ToInt32(distributed));
+            m_kernel.Run(outputPtr, 0, input1, input2, m_buffer, input2.Count, Convert.ToInt32(segmented), Convert.ToInt32(distributed));
         }
 
-        public void RunAsync(CudaStream stream, MyMemoryBlock<T> output, MyMemoryBlock<T> input1, MyMemoryBlock<T> input2, MyMemoryBlock<T> buffer)
+        public void RunAsync(CudaStream stream, MyMemoryBlock<T> output, MyMemoryBlock<T> input1, MyMemoryBlock<T> input2)
         {
-            CUdeviceptr outputPtr = output.GetDevicePtr(nGPU, outOffset * (m_outTypeSize / TSize), timeOffset);
+            CUdeviceptr outputPtr = output.GetDevicePtr(m_nGPU, outOffset * (m_outTypeSize / m_TSize), timeOffset);
 
-            //MyMemoryManager.Instance.CreateMemoryBlock<float>()
-
-            //void* rawOut, unsigned int outOff, volatile const void* rawIn1, volatile const void* rawIn2, void* tempBuffer, unsigned int size, bool segmented, bool distributed
-            m_kernel.RunAsync(stream, outputPtr, buffer, input1, input2, 0, input2.Count, Convert.ToInt32(segmented), Convert.ToInt32(distributed));
+            m_kernel.RunAsync(stream, outputPtr, 0, input1, input2, m_buffer, input2.Count, Convert.ToInt32(segmented), Convert.ToInt32(distributed));
         }
-
-        protected void SetupExecution(int numOfParallelUnits) { }
-        protected virtual void SetupExecution(dim3 blockDimensions, dim3 gridDimensions) { }
     }
 
+    public class ParallelKernelDescriptor : System.Attribute
+    {
+        public string name;
+        public int inTypeSize;
+        public int outTypeSize;
+
+        public ParallelKernelDescriptor(string name, int inTypeSize, int outTypeSize)
+        {
+            this.name = name;
+            this.inTypeSize = inTypeSize;
+            this.outTypeSize = outTypeSize;
+        }
+    }
+
+        
     /// <summary>
     /// OBSOLETE WILL BE DELETED SOON
     /// </summary>
