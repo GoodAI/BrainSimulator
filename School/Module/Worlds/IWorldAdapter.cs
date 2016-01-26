54e7dd04579eb006e4c09ac7aa1fe42bc1c64a53
MartinBalek <martin.balek@keenswh.com>
2016-01-26 14:47:40 +0100

    scaffolding for adapter - dynamic loading of WorldAdapters

---
+++ School/Module/Worlds/IWorldAdapter.cs
@@ -0,0 +1,93 @@
+﻿using GoodAI.Core.Nodes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace GoodAI.Modules.School.Worlds
+{
+    public interface IWorldAdapter
+    {
+        MyWorld World { get; }
+    }
+
+    public class AAWorld: MyWorld, IWorldAdapter
+    {
+        public MyWorld World {
+            get
+            {
+                return this;
+            }
+        }
+
+        public override void UpdateMemoryBlocks()
+        {
+        }
+    }
+
+    public class BBWorld : MyWorld, IWorldAdapter
+    {
+        public MyWorld World
+        {
+            get
+            {
+                return this;
+            }
+        }
+
+        public override void UpdateMemoryBlocks()
+        {
+        }
+    }
+
+    public static class IWorldAdaprersList
+    {
+        public static Type[] Types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => typeof(IWorldAdapter).IsAssignableFrom(p) && p.IsClass).ToArray();
+    }
+    
+    public class IWorldAdapterConverter: TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                return (IWorldAdapter)(Activator.CreateInstance(Type.GetType((string)value))); 
+            }
+            if (value == null)
+            {
+                return "";
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+        public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+        {
+            //Call the abstract GetValues function here.
+            return new StandardValuesCollection(GetValues());
+        }
+        public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+        {
+            return true;
+        }
+
+        protected Type[] GetValues() 
+        {
+            return IWorldAdaprersList.Types;
+        }
+    }
+}
---
+++ School/Module/Worlds/SchoolWorldAdapter.cs
@@ -0,0 +1,108 @@
+﻿using GoodAI.Core.Execution;
+using GoodAI.Core.Memory;
+using GoodAI.Core.Nodes;
+using GoodAI.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YAXLib;
+
+namespace GoodAI.Modules.School.Worlds
+{
+
+    public class SchoolWorldAdapter : MyWorld //, IMyCustomExecutionPlanner
+    {
+        [MyInputBlock]
+        public MyMemoryBlock<float> ActionInput
+        {
+            get { return GetInput(0); }
+        }
+
+        [MyOutputBlock(0)]
+        public MyMemoryBlock<float> Visual
+        {
+            get { return GetOutput(0); }
+            set { SetOutput(0, value); }
+        }
+
+        [MyOutputBlock(1)]
+        public MyMemoryBlock<float> Audio
+        {
+            get { return GetOutput(1); }
+            set { SetOutput(1, value); }
+        }
+
+        [MyOutputBlock(2)]
+        public MyMemoryBlock<float> Text
+        {
+            get { return GetOutput(2); }
+            set { SetOutput(2, value); }
+        }
+
+        [MyOutputBlock(3)]
+        public MyMemoryBlock<float> Data
+        {
+            get { return GetOutput(3); }
+            set { SetOutput(3, value); }
+        }
+
+        [MyBrowsable, Category("World Sizes")]
+        [YAXSerializableField(DefaultValue = 10000)]
+        public int VisualSize { get; set; }
+
+        [MyBrowsable, Category("World Sizes")]
+        [YAXSerializableField(DefaultValue = 1000)]
+        public int AudioSize { get; set; }
+
+        [MyBrowsable, Category("World Sizes")]
+        [YAXSerializableField(DefaultValue = 100)]
+        public int TextSize { get; set; }
+
+        [MyBrowsable, Category("World Sizes")]
+        [YAXSerializableField(DefaultValue = 200)]
+        public int DataSize { get; set; }
+
+        // for serialization
+        private string CurrentWorldType
+        {
+            get
+            {
+                if (CurrentWorld != null)
+                {
+                    return CurrentWorldType.ToString();
+                }
+                else
+                {
+                    return String.Empty;
+                }
+            }
+            set 
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    CurrentWorld = null;
+                }
+                else
+                {
+                    CurrentWorld = (IWorldAdapter)Type.GetType(value);
+                }
+            }
+        }
+
+        [MyBrowsable, Category("Awsome")]
+        [YAXDontSerialize, TypeConverter(typeof(IWorldAdapterConverter))]
+        public IWorldAdapter CurrentWorld { get; set; }
+        
+        public override void UpdateMemoryBlocks()
+        {
+            Visual.Count = VisualSize;
+            Audio.Count = AudioSize;
+            Text.Count = TextSize;
+            Data.Count = DataSize;
+        }
+
+    }
+}
