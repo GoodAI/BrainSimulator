49912371adbf1e4e8cad4d0f04cd6339d992b23f
Michal Vlasák <michal.vlasak@keenswh.com>
2016-02-03 17:04:54 +0100

    School GUI - close and reopen bug fixed

--- School/SchoolGUI/SchoolMainForm.Designer.cs
+++ School/SchoolGUI/SchoolMainForm.Designer.cs
@@ -254,6 +254,7 @@
             this.Controls.Add(this.btnImport);
             this.Controls.Add(this.checkBoxAutosave);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HideOnClose = true;
             this.Name = "SchoolMainForm";
             this.Text = "SchoolMainForm";
             this.Load += new System.EventHandler(this.SchoolMainForm_Load);
