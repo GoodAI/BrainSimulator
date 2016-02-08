471e0cd227316cd71de396ccf8537d9845887960
simon-andersson <simon.andersson@keenswh.com>
2016-02-08 15:17:42 +0100

    Remove some comments

--- School/Module/Learning tasks/LTDetectColor.cs
+++ School/Module/Learning tasks/LTDetectColor.cs
@@ -8,7 +8,7 @@ namespace GoodAI.Modules.School.LearningTasks
 
     public class LTDetectColor : AbstractLearningTask<RoguelikeWorld>
     {
-        private static readonly TSHintAttribute NUMBER_OF_COLORS = new TSHintAttribute("Condition salience", "", TypeCode.Single, 0, 1); //check needed;
+        private static readonly TSHintAttribute NUMBER_OF_COLORS = new TSHintAttribute("Condition salience", "", TypeCode.Single, 0, 1); 
 
         protected GameObject m_target;
         protected Random m_rndGen = new Random();
@@ -97,110 +97,4 @@ namespace GoodAI.Modules.School.LearningTasks
         }
 
     }
-
-
-    /*
-    public abstract class AbstractWorldAdapter<WorldType> : IWorldAdapter where WorldType : ManInWorld
-    {
-        protected Random m_rndGen = new Random();
-        protected WorldType m_w;
-
-        protected virtual WorldType World
-        {
-            get { return m_w; }
-        }
-
-        protected virtual void InstallWorld(WorldType w, TrainingSetHints trainingSetHints)
-        {
-            m_w = w;
-            m_w.ClearWorld();
-            if (trainingSetHints[TSHintAttributes.NOISE] > 0)
-            {
-                m_w.IsImageNoise = true;
-            }
-        }
-
-        public virtual void PresentNewTrainingUnit(AbstractSchoolWorld w, TrainingSetHints hints)
-        {
-            InstallWorld(w as WorldType, hints);
-        }
-
-        public virtual bool IsTrainingUnitCompleted(ref bool wasUnitSuccessful)
-        {
-            if (World.IsEmulatingUnitCompletion())
-            {
-                return World.EmulateIsTrainingUnitCompleted(out wasUnitSuccessful);
-            }
-            else
-            {
-                wasUnitSuccessful = true;
-                return true;
-            }
-        }
-    }*/
-
-    /*
-    public abstract class AbstractWADetectColor : IWorldAdapter
-    {
-        protected abstract AbstractSchoolWorld World { get; }
-
-
-        public void PresentNewTrainingUnit(AbstractSchoolWorld w, TrainingSetHints hints)
-        {
-            InstallWorld(w, hints);
-            CreateTarget(hints);
-            SetTargetColor();
-        }
-
-
-        protected abstract void InstallWorld(AbstractSchoolWorld w, TrainingSetHints trainingSetHints);
-        protected abstract void CreateTarget(TrainingSetHints trainingSetHints);
-    }
-
-    public class PlumberWorldWADetectColor : AbstractWorldAdapter<PlumberWorld>
-    {
-        protected GameObject m_target;
-
-        public override void PresentNewTrainingUnit(AbstractSchoolWorld w, TrainingSetHints hints)
-        {
-            CreateTarget(hints);
-            SetTargetColor();
-        }
-
-        protected void SetTargetColor()
-        {
-            m_target.isBitmapAsMask = true;
-            LearningTaskHelpers.RandomizeColor(ref m_target.maskColor, m_rndGen);
-        }
-
-        protected void CreateTarget(TrainingSetHints trainingSetHints)
-        {
-            m_target = new GameObject(GameObjectType.None, @"White10x10.png", 0, 0);
-            m_w.AddGameObject(m_target);
-            // POW is assumed to be centered
-            int minX = (m_w.FOW_WIDTH - m_w.POW_WIDTH) / 2;
-            int maxX = (m_w.FOW_WIDTH + m_w.POW_WIDTH) / 2 - m_target.Width;
-            m_target.X = m_rndGen.Next(minX, maxX + 1);
-            m_target.Y = (m_w.FOW_HEIGHT + m_w.POW_HEIGHT) / 2 - m_target.Height;
-        }
-    }
-
-    public class RoguelikeWorldWADetectColor : AbstractWorldAdapter<RoguelikeWorld>
-    {
-        protected GameObject m_target;
-
-        protected void CreateTarget(TrainingSetHints trainingSetHints)
-        {
-            m_target = new GameObject(GameObjectType.None, @"White10x10.png", 0, 0);
-            m_w.AddGameObject(m_target);
-            // POW is assumed to be centered
-            int minX = (m_w.FOW_WIDTH - m_w.POW_WIDTH) / 2;
-            int maxX = (m_w.FOW_WIDTH + m_w.POW_WIDTH) / 2 - m_target.Width;
-            m_target.X = m_rndGen.Next(minX, maxX + 1);
-            int minY = (m_w.FOW_HEIGHT - m_w.POW_HEIGHT) / 2;
-            int maxY = (m_w.FOW_HEIGHT + m_w.POW_HEIGHT) / 2 - m_target.Height;
-            m_target.Y = m_rndGen.Next(minY, maxY + 1);
-        }
-    }
-    */
 }
