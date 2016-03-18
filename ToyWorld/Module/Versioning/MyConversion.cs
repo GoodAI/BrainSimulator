518b5995058c54bcb83cdc81d7577da53fbe2cc8
Michal Vlasák <michal.vlasak@keenswh.com>
2016-03-18 16:02:46 +0100

    ToyWorld - initial commit

---
+++ ToyWorld/Module/Versioning/MyConversion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.Core.Configuration;
+
+namespace MyCompany.Modules.Versioning
+{
+    public class MyConversion: MyBaseConversion
+    {
+        public override int CurrentVersion
+        {
+            get { return 1; }
+        }
+    }
+}
