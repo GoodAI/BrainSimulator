81069cb78423208a518458a8cceae23d76d14c72
Michal Vlasák <michal.vlasak@keenswh.com>
2016-03-22 16:32:47 +0100

    ToyWorld tests - simple test added

---
+++ ToyWorld/ToyWorldTests/World/TileTests.cs
@@ -0,0 +1,22 @@
+﻿using World.Tiles;
+using Xunit;
+
+namespace ToyWorldTests.World
+{
+    public class TileTests
+    {
+        [Fact]
+        public void PickaxeDamageWall()
+        {
+            // Arrange
+            Wall wall = new Wall();
+            GameAction pickaxe = new UsePickaxe();
+
+            // Act
+            var pickaxedWall = wall.TransformTo(pickaxe);
+
+            // Assert
+            Assert.IsType(typeof(DamagedWall), pickaxedWall);
+        }
+    }
+}
