92cda94059f3d20e3b04dcb074d74db7e59dd686
Martin Stránský <martin.stransky@keenswh.com>
2016-03-22 12:16:48 +0100

    Solution repair

---
+++ ToyWorld/World/Tiles/AbstractTile.cs
@@ -0,0 +1,34 @@
+﻿namespace GoodAI.ToyWorldAPI.Tiles
+{
+    /// <summary>
+    /// All tiles (objects fixed to the grid) are derived from this abstract class.
+    /// </summary>
+    public abstract class AbstractTile
+    {
+        /// <summary>
+        /// TileType is number in tsx tileset
+        /// </summary>
+        public abstract int TileType { get; }
+    }
+
+    /// <summary>
+    /// StaticTile is tile, which cannot be updated, but can be replaced by dynamic tile. Only one static 
+    /// </summary>
+    public abstract class StaticTile : AbstractTile { }
+
+    /// <summary>
+    /// DynamicTile is tile with internal state that can
+    /// </summary>
+    public abstract class DynamicTile : AbstractTile
+    {
+        /// <summary>
+        /// Method will be called once upon a time according to previus registration
+        /// </summary>
+        public abstract void Update();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public abstract void RegisterForUpdate();
+    }
+}
---
+++ ToyWorld/World/Tiles/Wall.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using GoodAI.ToyWorldAPI.Tiles;
+
+namespace World.Tiles
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Wall : StaticTile
+    {
+        private static int _tileSet;
+        public override int TileType
+        {
+            get
+            {
+                if (_tileSet == 0)
+                {
+                    _tileSet = TileSetTableParser.TileNumber("Wall");
+                }
+                return _tileSet;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DamagedWall : DynamicTile
+    {
+        public override int TileType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RegisterForUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DestroyedWall : StaticTile
+    {
+        public override int TileType
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
+
