10895d3a33186017b4080fcf876f19a10e0bfb13
Martin Milota <martin.milota@keenswh.com>
2016-03-24 17:30:27 +0100

    Utils rename

---
+++ ToyWorld/Utils/VRageRIP/Lib/Collections/IterableQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.Collections;
+
+namespace VRage.Collections
+{
+    public class IterableQueue<T> : MyQueue<T>, IEnumerable<T>
+    {
+        public IterableQueue()
+            : base(200)
+        { }
+
+        public IterableQueue(int capacity)
+            : base(capacity)
+        { }
+
+        public IterableQueue(IEnumerable<T> collection)
+            : base(collection)
+        { }
+
+
+        #region IEnumerable<> overrides
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (m_head < m_tail)
+            {
+                for (int i = m_head; i < m_tail; i++)
+                    yield return m_array[i];
+            }
+            else
+            {
+                for (int i = m_head; i < m_array.Length; i++)
+                    yield return m_array[i];
+
+                for (int i = 0; i < m_tail; i++)
+                    yield return m_array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
