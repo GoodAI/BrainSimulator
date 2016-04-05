a9badfa366ffe91ca0e94caa060271170a28d0bb
Martin Stránský <martin.stransky@keenswh.com>
2016-04-05 11:02:22 +0200

    MapLoaderTest improved

--- ToyWorld/ToyWorldTests/World/MapLoaderTest.cs
+++ ToyWorld/ToyWorldTests/World/MapLoaderTest.cs
@@ -9,9 +9,13 @@ namespace ToyWorldTests.World
 {
     public class MapLoaderTests
     {
-        private Atlas m_atlas;
+        /// <summary>
+        /// Loads map from testing files
+        /// </summary>
+        private readonly Atlas m_atlas;
         public MapLoaderTests()
         {
+            // initiate streamReaders
             var tmxMemoryStream = TestingFiles.Files.GetTmxMemoryStream();
             var tilesetTableMemoryStream = TestingFiles.Files.GetTilesetTableMemoryStream();
 
@@ -20,13 +24,18 @@ namespace ToyWorldTests.World
 
             var tilesetTable = new TilesetTable(tilesetTableStreamReader);
 
+            // create atlas
             m_atlas = MapLoader.LoadMap(tmxStreamReader, tilesetTable);
         }
 
         [Fact]
         public void MapCanLoad()
         {
-            Assert.True(true);
+            Assert.NotNull(m_atlas);
+            // at least 1 static tile expected
+            Assert.True(m_atlas.StaticTilesContainer.Count > 0);
+            // at least 7 tile layers
+            Assert.True(m_atlas.TileLayers.Count >= 7);
         }
     }
 }
