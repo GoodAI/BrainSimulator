38f62181fcadc2a1e1ed15f1dac6e143c6f83529
Jaroslav Vitku <jaroslav.vitku@keenswh.com>
2016-04-06 15:22:58 +0200

    added conversion of the MNISTModule where SendTrainingData replaces SendData
      ..so that old brains can be opened and their behavior is not changed

--- Sources/Modules/MNIST/Module/Versioning/MyConversion.cs
+++ Sources/Modules/MNIST/Module/Versioning/MyConversion.cs
@@ -6,7 +6,19 @@ namespace MNIST.Versioning
     {
         public override int CurrentVersion
         {
-            get { return 1; }
+            get { return 2; }
+        }
+
+        /// <summary>
+        /// Convert MNISTWorld brains for the training and testing tasks. 
+        /// Enable training by default, so that the functionality is preserved.
+        /// Author: jv
+        /// </summary>
+        public static string Convert1To2(string xml)
+        {
+            xml = xml.Replace("MNIST.MySendMNISTTask", "MNIST.MySendTrainingMNISTTask");
+            xml = xml.Replace("SendMNISTData", "SendTrainingMNISTData");
+            return xml;
         }
     }
 }
