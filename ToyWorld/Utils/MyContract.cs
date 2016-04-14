0fe689b3f792dee53e240e1a36c0a1868077534c
Michal Vlasák <michal.vlasak@keenswh.com>
2016-04-14 07:44:08 +0200

    MyContract - explanation comment added

--- ToyWorld/Utils/MyContract.cs
+++ ToyWorld/Utils/MyContract.cs
@@ -3,11 +3,15 @@ using System.Diagnostics.Contracts;
 
 namespace Utils
 {
+    /// <summary>
+    /// Used for implementing Exception version of Reguires Code Contract for people without Code Contracts installed
+    /// </summary>
     public static class MyContract
     {
         public static void Requires<TException>(bool condition, string message = "") where TException : Exception, new()
         {
 #if CONTRACTS_FULL
+            // TODO: Message is missing because CC wants it to be known before build (it has to be static or string literal)
             Contract.Requires<TException>(condition);
 #else
             if (!condition) throw new TException();
