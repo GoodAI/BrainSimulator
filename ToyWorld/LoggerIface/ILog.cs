47c91a5492f83a882de5dd12aed372e5f4b326db
Martin Stránský <martin.stransky@keenswh.com>
2016-04-21 13:49:05 +0200

    Logger interface added

---
+++ ToyWorld/LoggerIface/ILog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodAI.Logging
+{
+    public enum Severity
+    {
+        Error,
+        Warn,
+        Info,
+        Debug,
+        Verbose
+    }
+
+    public interface ILog
+    {
+        void Add(Severity severity, string template, params object[] objects);
+        void Add(Severity severity, Exception ex, string template, params object[] objects);
+    }
+}
---
+++ ToyWorld/LoggerIface/LoggerExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodAI.Logging
+{
+    public static class LoggerExtensions
+    {
+        public static void Error(this ILog log, string template, params object[] objects)
+        {
+            log.Add(Severity.Error, template, objects);
+        }
+
+        public static void Error(this ILog log, Exception ex, string template, params object[] objects)
+        {
+            log.Add(Severity.Error, ex, template, objects);
+        }
+
+        public static void Warn(this ILog log, string template, params object[] objects)
+        {
+            log.Add(Severity.Warn, template, objects); 
+        }
+
+        public static void Warn(this ILog log, Exception ex, string template, params object[] objects)
+        {
+            log.Add(Severity.Warn, ex, template, objects);
+        }
+
+        public static void Info(this ILog log, string template, params object[] objects)
+        {
+            log.Add(Severity.Info, template, objects);
+        }
+
+        public static void Debug(this ILog log, string template, params object[] objects)
+        {
+            log.Add(Severity.Debug, template, objects);
+        }
+
+        public static void Debug(this ILog log, Exception ex, string template, params object[] objects)
+        {
+            log.Add(Severity.Debug, ex, template, objects);
+        }
+    }
+}
