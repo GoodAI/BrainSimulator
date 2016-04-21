25a5e9194a9517c38f2fbc51c94549470e7d5378
Michal Vlasák <michal.vlasak@keenswh.com>
2016-04-21 12:50:30 +0200

    ToyWorld - covariant added

--- ToyWorld/World/ToyWorldCore/ILayer.cs
+++ ToyWorld/World/ToyWorldCore/ILayer.cs
@@ -3,7 +3,7 @@ using World.GameActors;
 
 namespace World.ToyWorldCore
 {
-    public interface ILayer<T> where T : GameActor
+    public interface ILayer<out T> where T : GameActor
     {
         LayerType LayerType { get; set; }
     }
