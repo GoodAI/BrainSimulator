86cc0a3aea176c4427456829e9e288766cf02172
MartinHyben <martin.hyben@keenswh.com>
2016-04-25 16:08:00 +0200

    Added Dynamic Boltzman Machine (DyBM) module.

---
+++ Sources/Modules/BasicNodes/Module/DyBM/MyDyBMInputLayer.cs
@@ -0,0 +1,264 @@
+﻿using GoodAI.Core.Memory;
+using GoodAI.Core.Nodes;
+using GoodAI.Core.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using YAXLib;
+using System.Linq;
+using GoodAI.Modules.DyBM.Tasks;
+
+namespace GoodAI.Modules.DyBM
+{
+    public class MyQueue : Queue<float>
+    {
+        public MyQueue(int capacity)
+        :base(capacity)
+        {
+            for (int i = 0; i < capacity; i++)
+                this.Enqueue(0);
+        }
+
+        public float Head()
+        {
+            return this.Peek();
+        }
+
+        public float Tail()
+        {
+            return this.ToArray()[this.Count - 1];
+        }
+    }
+
+    public class MyDyBMInputLayer : MyWorkingNode
+    {
+        // Inputs and Outputs
+        /// <summary>
+        /// Input of the Layer
+        /// </summary>
+        [MyInputBlock(0)]
+        public MyMemoryBlock<float> Input 
+        { 
+            get { return GetInput(0); } 
+        }
+
+        /// <summary>
+        /// Output of the Layer
+        /// </summary>
+        [MyOutputBlock(0)]
+        public MyMemoryBlock<float> Output
+        {
+            get { return GetOutput(0); }
+            set { SetOutput(0, value); }
+        }
+
+        // Public properties
+        /// <summary>
+        /// Slope of LTD eligibility trace 
+        /// </summary>
+        [MyBrowsable, Category("Properties")]
+        [YAXSerializableField(DefaultValue = "0.5"), YAXElementFor("Properties")]
+        public string Slope_μ 
+        {
+            get {
+                string value = "";
+
+                for (int n = 0; n < slope_μ.Count; n++)
+                    value += slope_μ[n].ToString() + " ";
+
+                value = value.Remove(value.Length - 1, 1);
+                return value;
+                }
+
+            set {
+                    slope_μ = (value.Length > 0)
+                    ? value.Trim().Split(',', ' ').Select(a => float.Parse(a, CultureInfo.InvariantCulture)).ToList()
+                    : null;
+
+                    for (int i = 0; i < slope_μ.Count; i++)
+                    {
+                        slope_μ[i] = slope_μ[i] <= 0 ? 0.1f : slope_μ[i];
+                        slope_μ[i] = slope_μ[i] >= 1 ? 0.9f : slope_μ[i];
+                        
+                    }
+                }
+        }
+        public List<float> slope_μ;
+
+        /// <summary>
+        /// Slope of LTP eligibility trace
+        /// </summary>
+        [MyBrowsable, Category("Properties")]
+        [YAXSerializableField(DefaultValue = "0.5"), YAXElementFor("Properties")]
+        public string Slope_λ 
+        {
+            get
+            {
+                string value = "";
+
+                for (int n = 0; n < slope_λ.Count; n++)
+                    value += slope_λ[n].ToString() + " ";
+
+                value = value.Remove(value.Length - 1, 1);
+                return value;
+            }
+
+            set
+            {
+                slope_λ = (value.Length > 0)
+                  ? value.Trim().Split(',', ' ').Select(a => float.Parse(a, CultureInfo.InvariantCulture)).ToList()
+                  : null;
+
+                for (int i = 0; i < slope_λ.Count; i++)
+                {
+                    slope_λ[i] = slope_λ[i] <= 0 ? 0.1f : slope_λ[i];
+                    slope_λ[i] = slope_λ[i] >= 1 ? 0.9f : slope_λ[i];
+                    
+                }
+            }
+        }
+        public List<float> slope_λ;
+
+        /// <summary>
+        /// Interval of axonal conduction delays. The actual conduction delay of each FIFO queue is sampled from this interval.
+        /// </summary>
+        [MyBrowsable, Category("Properties")]
+        [YAXSerializableField(DefaultValue = 9), YAXElementFor("Properties")]
+        public int DelayInterval { get; set; }
+
+        /// <summary>
+        /// Initial Learning rate η0 of the Layer
+        /// </summary>
+        [MyBrowsable, Category("Properties")]
+        [YAXSerializableField(DefaultValue = 1.0f), YAXElementFor("Properties")]
+        public float LearningRate_η { get; set; }
+
+        /// <summary>
+        /// Temperature of the Layer
+        /// </summary>
+        [MyBrowsable, Category("Properties")]
+        [YAXSerializableField(DefaultValue = 1.0f), YAXElementFor("Properties")]
+        public float Temperature_τ { get; set; }
+
+        /// <summary>
+        /// Number of iterations for log-likelihood collection until learning rate update
+        /// </summary>
+        [MyBrowsable, Category("Properties")]
+        [YAXSerializableField(DefaultValue = 3), YAXElementFor("Properties")]
+        public int LearningRateCorection_M { get; protected set; }
+
+        // Observable MemoryBlocks
+        /// <summary>
+        /// One FIFO queue for each synapse between neurons
+        /// </summary>
+        public MyQueue[] Fifo_x { get; protected set; }
+        /// <summary>
+        /// Each neurons bias. Large positive bias makes neuron most likely to spike.
+        /// </summary>
+        public MyMemoryBlock<float> Bias_b { get; protected set; }
+        /// <summary>
+        /// Energy of each neuron at time t
+        /// </summary>
+        public MyMemoryBlock<float> Energy_E { get; protected set; }
+        /// <summary>
+        /// Conduction delay of each synapse
+        /// </summary>
+        public MyMemoryBlock<float> Delay_d { get; protected set; }
+        /// <summary>
+        /// One Long-Term Potentiation weight for each synapse
+        /// </summary>
+        public MyMemoryBlock<float> Weight_u { get; protected set; }
+        /// <summary>
+        /// One Long-Term Depresion weight for each synapse
+        /// </summary>
+        public MyMemoryBlock<float> Weight_v { get; protected set; }
+        /// <summary>
+        /// Long-Term Potentiation eligibility trace
+        /// </summary>
+        public MyMemoryBlock<float> Trace_α { get; protected set; }
+        /// <summary>
+        /// Long-Term Depresion eligibility trace, only considering spikes within period of Conduction delay
+        /// </summary>
+        public MyMemoryBlock<float> Trace_β { get; protected set; }
+        /// <summary>
+        /// Long-Term Depresion eligibility trace, considering spikes after Conduction delay
+        /// </summary>
+        public MyMemoryBlock<float> Trace_γ { get; protected set; }
+        
+        // Public parameters
+        /// <summary>
+        /// Number of neurons within the Layer
+        /// </summary>
+        public int Neurons { get; protected set; }
+        /// <summary>
+        /// Number of synapses within the Layer
+        /// </summary>
+        public int Synapses { get; protected set; }
+        /// <summary>
+        /// Number of eligibility traces for each synapse based on parameters
+        /// </summary>
+        public int Traces_K { get; protected set; }
+        /// <summary>
+        /// Number of eligibility traces for each neuron based on parameters
+        /// </summary>
+        public int Traces_L { get; protected set; }
+        /// <summary>
+        /// Log-likelihood derivate for Learning rate update
+        /// </summary>
+        public float Derivative_Δ { get; protected set; }
+        private Random rand = new Random();
+
+        // Tasks
+        public MyDyBMLearningTask LearningTask { get; protected set; }
+
+        /// <summary>
+        /// Computes a Factorial of int number
+        /// </summary>
+        public static Func<int, int> Factorial = x => x < 0 ? -1 : x == 1 || x == 0 ? 1 : x * Factorial(x - 1);
+
+        /// <summary>
+        /// Constructor of DyBMInputLayer class
+        /// </summary>
+        public MyDyBMInputLayer() { }
+
+        public override void UpdateMemoryBlocks()
+        {
+            if(Input != null)
+            {
+                // Number of Neurons on simulation start is equal to number of inputs
+                Neurons = Input.Count;
+
+                // Fully connected network
+                Synapses = Neurons * Neurons;
+
+                // Set num. of outputs equal to num. of neurons
+                Output.Count = Neurons;
+
+                // Init numbers of eligibility traces according to parameters
+                Traces_K = slope_λ.Count;
+                Traces_L = slope_μ.Count;
+
+                // Init Memory blocks
+                Bias_b.Count = Neurons;
+                Energy_E.Count = Neurons;
+                Delay_d.Count = Synapses;
+                Weight_u.Count = Synapses * Traces_K;
+                Weight_v.Count = Synapses * Traces_L;
+                Trace_α.Count = Synapses * Traces_K;
+                Trace_β.Count = Synapses * Traces_K;
+                Trace_γ.Count = Neurons * Traces_L;
+
+                // Create N FIFO queues and initialize their conduction delays
+                Fifo_x = new MyQueue[Synapses];
+                for (int n = 0; n < Synapses; n++)
+                {
+                    int capacity = rand.Next(1, DelayInterval);
+                    Fifo_x[n] = new MyQueue(capacity);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Sources/Modules/BasicNodes/Module/DyBM/Tasks/MyDyBMLearningTask.cs
@@ -0,0 +1,257 @@
+﻿using GoodAI.Core.Task;
+using GoodAI.Core.Utils;
+using System;
+using System.ComponentModel;
+
+namespace GoodAI.Modules.DyBM.Tasks
+{
+    /// <summary>
+    /// Layer learning task
+    /// </summary>
+    [Description("DyBM Learning"), MyTaskInfo(OneShot = false)]
+    public class MyDyBMLearningTask : MyTask<MyDyBMInputLayer>
+    {
+        /// <summary>
+        /// Initialize all parameters to 0 -> meaning that previous sequences were all 0
+        /// </summary>
+        /// <param name="nGPU"></param>
+        public override void Init(int nGPU)
+        {
+            if (Owner.Delay_d != null)
+            {
+                Random rand = new Random();
+                Owner.Delay_d.SafeCopyToHost();
+                Owner.Bias_b.SafeCopyToHost();
+                Owner.Weight_u.SafeCopyToHost();
+                Owner.Weight_v.SafeCopyToHost();
+
+                for (int s = 0; s < Owner.Neurons; s++)
+                {
+                    // Randomize bias
+                    Owner.Bias_b.Host[s] = (float)rand.Next(1, 10) / 100;
+                }
+
+                for (int s = 0; s < Owner.Synapses; s++)
+                {
+                    // Init conduction delays
+                    Owner.Delay_d.Host[s] = Owner.Fifo_x[s].Count;
+                    // Randomize weights
+                    Owner.Weight_u.Host[s] = (float)rand.Next(1, 10) / 100;
+                    Owner.Weight_v.Host[s] = (float)rand.Next(1, 10) / 100;
+                }
+
+                Owner.Delay_d.SafeCopyToDevice();
+                Owner.Bias_b.SafeCopyToDevice();
+                Owner.Weight_u.SafeCopyToDevice();
+                Owner.Weight_v.SafeCopyToDevice();
+            }
+        }
+
+        /// <summary>
+        /// Execute one learning step of the layer through all neurons
+        /// </summary>
+        public override void Execute()
+        {
+            int L, K, M, m;
+            float head, tail;
+            float[] αij, βij, γj, uij, vij, Eθ, Ȇθ, μ, λ;
+
+            int Neurons = Owner.Neurons;
+            float τ = Owner.Temperature_τ;
+            float η = Owner.LearningRate_η;
+            float Δ = Owner.Derivative_Δ;
+            
+            m = 0;
+            M = Owner.LearningRateCorection_M;
+            K = Owner.Traces_K;
+            L = Owner.Traces_L;
+            Eθ = Owner.Energy_E.Host;
+
+            Owner.Output.SafeCopyToHost();
+            Owner.Trace_α.SafeCopyToHost();
+            Owner.Trace_β.SafeCopyToHost();
+            Owner.Trace_γ.SafeCopyToHost();
+            Owner.Bias_b.SafeCopyToHost();
+            Owner.Energy_E.SafeCopyToHost();
+            Owner.Weight_u.SafeCopyToHost();
+            Owner.Weight_v.SafeCopyToHost();
+
+
+            #region Take the input of the Layer
+            Owner.Input.SafeCopyToHost();
+            // Input of the at time t
+            float[] x = Owner.Input.Host;
+            // Expected input at time t
+            float[] X = new float[Neurons];
+            #endregion
+
+            #region Feed-Forward pass for all neurons
+            for (int j = 0; j < Neurons; j++)
+            {
+                // Define head and tail of each FIFO queue
+                tail = Owner.Fifo_x[j].Tail();
+                head = Owner.Fifo_x[j].Head();
+                
+                μ = Owner.slope_μ.ToArray();
+                λ = Owner.slope_λ.ToArray();
+
+                // Compute eligibility traces
+                float sum_α = 0;
+                float sum_β = 0;
+                float sum_γ = 0;
+
+                γj = new float[L];
+                Array.Copy(Owner.Trace_γ.Host, (j * L), γj, 0, L);
+                
+                #region γ - Recurrent connections
+                for (int l = 0; l < L; l++)
+                {
+                    γj[l] = μ[l] * (γj[l] + tail);
+                    Owner.Trace_γ.Host[(j * L) + l] = γj[l];
+                }
+                #endregion
+
+                #region Process all synapses of each neuron
+                for (int i = 0; i < Neurons; i++)
+                {
+                    // Index of the synapse
+                    int idx = (j * Neurons) + (i * K);
+
+                    // Pointers to subarrays relevant for this synapse
+                    αij = new float[K];
+                    Array.Copy(Owner.Trace_α.Host, idx, αij, 0, K);
+                    βij = new float[K];
+                    Array.Copy(Owner.Trace_β.Host, idx, βij, 0, K);
+                    uij = new float[K];
+                    Array.Copy(Owner.Weight_u.Host, idx, uij, 0, K);
+                    vij = new float[L];
+                    Array.Copy(Owner.Weight_v.Host, idx, vij, 0, L);
+
+                    #region α - Feed forward connections
+                    for (int k = 0; k < K; k++)
+                    {
+                        // Update α eligibil;ity trace
+                        float d = Owner.Delay_d.Host[(j * Neurons) + i];
+                        αij[k] = λ[k] * (αij[k] + head);
+                        Owner.Trace_α.Host[idx] = αij[k];
+
+                        // Weight sum for Energy computation
+                        sum_α += uij[k] * αij[k] * x[j];
+                    }
+                    #endregion
+
+                    #region β - Only remembered recurrent connections
+                    // FIFO indexes
+                    int _tail = 0;
+                    int _head = Owner.Fifo_x[i].Count - 1;
+
+                    // Update β eligibility trace - reset every iteration
+                    for (int l = 0; l < L; l++)
+                    {
+                        βij[l] = 0;
+                        for (int s = _head; s > _tail; s--)
+                        {
+                            float xi = Owner.Fifo_x[i].ToArray()[s];
+                            βij[l] += (float)Math.Pow(μ[l], -s) * xi;
+                        }
+
+                        Owner.Trace_β.Host[idx] = βij[l];
+
+                        // Weight sums for Energy computation
+                        sum_β += vij[l] * βij[l] * x[j];
+                        sum_γ += vij[l] * μ[l] * x[j];
+                    }
+                    #endregion
+                }
+                #endregion
+
+                #region Compute Energy, Log-likelihood and next Expected input
+                // Compute Energy of each neuron
+                Eθ[j] = -Owner.Bias_b.Host[j] * x[j] - sum_α + sum_β + sum_γ;
+                Owner.Energy_E.Host[j] = Eθ[j];
+
+                // Compute next input estimate denoted as Pθ
+                X[j] = (float)(Math.Exp(Math.Pow(-τ, -1) * Eθ[j]) /
+                  (1 + (float)(Math.Exp(Math.Pow(-τ, -1) * Eθ[j]) )));
+                
+                if (float.IsNaN(X[j]))
+                    X[j] = 0;
+
+                // Compute a log likelihood
+                float logPθ = (float)Math.Log10(X[j]);
+                #endregion
+
+                #region Update bias and all synapses of each neuron
+                // Update bias value
+                Owner.Bias_b.Host[j] += η * (x[j] - X[j]);
+                
+                // Update all synapses
+                for (int i = 0; i < Neurons; i++)
+                {
+                    // Index of the synapse
+                    int idx = (j * Neurons) + (i * K);
+
+                    // Pointers to subarrays relevant for this synapse
+                    αij = new float[K];
+                    Array.Copy(Owner.Trace_α.Host, idx, αij, 0, K);
+                    βij = new float[K];
+                    Array.Copy(Owner.Trace_β.Host, idx, βij, 0, K);
+
+                    // Update learning rate
+                    if (m < M)
+                    {
+                        Δ += logPθ * logPθ;
+                    }
+                    else
+                    {
+                        Owner.LearningRate_η = η / ((float)Math.Sqrt(Δ));
+                        Δ = 0;
+                        m = 1;
+                    }
+
+                    // Update LTP weight
+                    for (int k = 0; k < K; k++)
+                    {
+                        Owner.Weight_u.Host[idx] += η * (x[j] - X[j]) * αij[k];
+                    }
+
+                    // Update first part of LTD weight
+                    for (int l = 0; l < L; l++)
+                    {
+                        Owner.Weight_v.Host[idx] += η * (X[j] - x[j]) * βij[l];
+                    }
+
+                    // Update second part of LTD weight
+                    for (int l = 0; l < L; l++)
+                    {
+                        Owner.Weight_v.Host[idx] += η * (X[j] - x[i]) * γj[l];
+                    }
+                }
+                #endregion
+            }
+            #endregion
+
+            #region Manage FIFO queues
+            for (int j = 0; j < Neurons; j++)
+            {
+                // Insert new value to the queue
+                Owner.Fifo_x[j].Enqueue(x[j]);
+                // Discard the head value of the queue
+                Owner.Fifo_x[j].Dequeue();
+            }
+            #endregion
+
+            X.CopyTo(Owner.Output.Host, 0);
+            
+
+            Owner.Output.SafeCopyToDevice();
+            Owner.Trace_α.SafeCopyToDevice();
+            Owner.Trace_β.SafeCopyToDevice();
+            Owner.Trace_γ.SafeCopyToDevice();
+            Owner.Bias_b.SafeCopyToDevice();
+            Owner.Energy_E.SafeCopyToDevice();
+            Owner.Weight_u.SafeCopyToDevice();
+            Owner.Weight_v.SafeCopyToDevice();
+        }
+    }
+}
