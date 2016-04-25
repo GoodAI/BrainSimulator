51bc4af95c811adf9715aab626fd9cdb09810b2b
Michal Vlasák <michal.vlasak@keenswh.com>
2016-04-25 15:38:00 +0200

    ToyWorld - LayerType changed

--- ToyWorld/World/ToyWorldCore/LayerType.cs
+++ ToyWorld/World/ToyWorldCore/LayerType.cs
@@ -1,20 +1,26 @@
-﻿namespace World.ToyWorldCore
+﻿using System;
+
+namespace World.ToyWorldCore
 {
 
     /// <summary>
     /// List of all Layers. Layers are loaded according to names; number determines order of rendering.
     /// We expect Avatar and Agent to be inside Object Layer.
     /// </summary>
+    [Flags]
     public enum LayerType
     {
         Background = 1,
         OnBackground = 2,
-        Path = 3,
-        OnGroundInteractable = 4,
-        ObstacleInteractable = 5,
-        Obstacle = 6,
-        Object = 7,
-        Foreground = 8,
-        ForegroundObject = 9
+        Path = 4,
+        OnGroundInteractable = 8,
+        ObstacleInteractable = 16,
+        Obstacle = 32,
+        Object = 64,
+        Foreground = 128,
+        ForegroundObject = 256,
+
+        Interactable = OnGroundInteractable | ObstacleInteractable,
+        All = Background | OnBackground | Path | OnGroundInteractable | ObstacleInteractable | Obstacle | Object | Foreground | ForegroundObject
     }
 }
