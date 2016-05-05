81b299ff2653e437fa5ca2a2577ce10392f683f6
Martin Stránský <martin.stransky@keenswh.com>
2016-05-05 14:45:22 +0200

    CollisionChecker Fix

--- ToyWorld/World/Physics/CollisionChecker.cs
+++ ToyWorld/World/Physics/CollisionChecker.cs
@@ -136,6 +136,7 @@ namespace World.Physics
                     {
                         firstList.UnionWith(secondList);
                         listOfSets.RemoveAt(j);
+                        j--;
                     }
                 }
             }
