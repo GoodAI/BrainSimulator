2a86f60b61bad6de98c4c7d872a1959cd49774ed
Martin Milota <martin.milota@keenswh.com>
2016-05-05 10:53:59 +0200

    Re-did Vbo inheritance hierarchy to better hide update functions for StaticVbo;

--- ToyWorld/Render/RenderObjects/Buffers/Base/Vbos.cs
+++ ToyWorld/Render/RenderObjects/Buffers/Base/Vbos.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using OpenTK.Graphics.OpenGL;
 
@@ -8,7 +9,7 @@ namespace Render.RenderObjects.Buffers
     {
         public int Handle { get; private set; }
 
-        public int ByteCount { get; private set; }
+        public int ByteCount { get; protected set; }
 
         public BufferTarget Target { get; private set; }
         public int ElementSize { get; private set; }
@@ -18,6 +19,8 @@ namespace Render.RenderObjects.Buffers
 
         protected VboBase(int elementSize = 4, BufferTarget target = BufferTarget.ArrayBuffer)
         {
+            Handle = GL.GenBuffer();
+
             Target = target;
             ElementSize = elementSize;
         }
@@ -27,33 +30,7 @@ namespace Render.RenderObjects.Buffers
             GL.DeleteBuffer(Handle);
         }
 
-        protected void Init<T>(int tCount, T[] data, BufferUsageHint hint)
-            where T : struct
-        {
-            int tSize = Marshal.SizeOf(typeof(T));
-
-            Handle = GL.GenBuffer();
-            ByteCount = tCount * tSize;
-
-            Bind();
-            GL.BufferData(Target, ByteCount, data, hint);
-            //Unbind();
-        }
-
         #endregion
-
-        public virtual void Update<T>(T[] data, int tCount = -1, int offset = 0)
-            where T : struct
-        {
-            var tSize = Marshal.SizeOf(typeof(T));
-
-            if (tCount == -1)
-                tCount = Math.Min(ByteCount, data.Length * tSize);
-
-            Bind();
-            GL.BufferSubData(Target, new IntPtr(offset * tSize), tCount, data);
-            //Unbind();
-        }
 
         public void Bind()
         {
@@ -66,17 +43,69 @@ namespace Render.RenderObjects.Buffers
         //}
     }
 
-    internal class Vbo<T> : VboBase
+    internal abstract class VboBase<T> : VboBase
         where T : struct
     {
-        public Vbo(int tCount, T[] initData = null, int elementSize = -1, BufferTarget target = BufferTarget.ArrayBuffer, BufferUsageHint hint = BufferUsageHint.DynamicDraw)
+        protected VboBase(int elementSize = -1, BufferTarget target = BufferTarget.ArrayBuffer, BufferUsageHint hint = BufferUsageHint.DynamicDraw)
+            : base(elementSize < 0 ? 4 : elementSize, target)
+        { }
+
+        protected VboBase(int tCount, T[] initData = null, int elementSize = -1, BufferTarget target = BufferTarget.ArrayBuffer, BufferUsageHint hint = BufferUsageHint.DynamicDraw)
             : base(elementSize < 0 ? 4 : elementSize, target)
         {
             Init(tCount, initData, hint);
         }
+
+
+        protected void Init(int tCount, T[] data = null, BufferUsageHint hint = BufferUsageHint.StaticRead)
+        {
+            Debug.Assert(tCount > 0, "Buffer count must be positive.");
+            int tSize = Marshal.SizeOf(typeof(T));
+
+            ByteCount = tCount * tSize;
+
+            Bind();
+            GL.BufferData(Target, ByteCount, data, hint);
+            //Unbind();
+        }
+
+        protected void Update(T[] data, int tCount = -1, int offset = 0)
+        {
+            var tSize = Marshal.SizeOf(typeof(T));
+
+            if (tCount == -1)
+                tCount = Math.Min(ByteCount, data.Length * tSize);
+
+            Bind();
+            GL.BufferSubData(Target, new IntPtr(offset * tSize), tCount, data);
+            //Unbind();
+        }
     }
 
-    internal sealed class StaticVbo<T> : Vbo<T>
+    internal class Vbo<T> : VboBase<T>
+        where T : struct
+    {
+        protected Vbo(int elementSize = -1, BufferTarget target = BufferTarget.ArrayBuffer, BufferUsageHint hint = BufferUsageHint.DynamicDraw)
+            : base(elementSize, target, hint)
+        { }
+
+        public Vbo(int tCount, T[] initData = null, int elementSize = -1, BufferTarget target = BufferTarget.ArrayBuffer, BufferUsageHint hint = BufferUsageHint.DynamicDraw)
+            : base(tCount, initData, elementSize, target, hint)
+        { }
+
+
+        public new void Init(int tCount, T[] data = null, BufferUsageHint hint = BufferUsageHint.StaticRead)
+        {
+            base.Init(tCount, data, hint);
+        }
+
+        public new void Update(T[] data, int tCount = -1, int offset = 0)
+        {
+            base.Update(data, tCount, offset);
+        }
+    }
+
+    internal sealed class StaticVbo<T> : VboBase<T>
         where T : struct
     {
         public StaticVbo(int tCount, T[] initData = null, int elementSize = -1, BufferTarget target = BufferTarget.ArrayBuffer, BufferUsageHint hint = BufferUsageHint.DynamicDraw)
@@ -86,12 +115,5 @@ namespace Render.RenderObjects.Buffers
         // These are shared by many geometries, prevent their disposal
         public override void Dispose()
         { }
-
-
-        public override void Update<T1>(T1[] data, int count = -1, int offset = 0)
-        {
-            // TODO: Allow it for some global changes in the future?
-            throw new NotImplementedException("Static data shan't be updated.");
-        }
     }
 }
