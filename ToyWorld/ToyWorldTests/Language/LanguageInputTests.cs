235c57966ba55fa8bca7283491a4edc576e7da10
simon-andersson <simon.andersson@keenswh.com>
2016-05-17 13:44:11 +0200

    Add test skeleton

---
+++ ToyWorld/ToyWorldTests/Language/LanguageInputTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ToyWorldTests.Language
+{
+    public class LanguageInputTests
+    {
+        // Creates a word vector
+        [Fact]
+        public void CreateVector()
+        {
+
+        }
+
+        // Creates different vectors for different words
+        [Fact]
+        public void CreateDifferentVector()
+        {
+
+        }
+
+        // Creates identical vectors for identical words
+        [Fact]
+        public void CreateIdenticalVector()
+        {
+
+        }
+
+        // Handles text with more tokens than the input
+        // layer can contain
+        [Fact]
+        public void InputTooManyTokens()
+        {
+
+        }
+
+        // Ensures the vectors after last word are zero
+        [Fact]
+        public void PutZerosAfterLastWord()
+        {
+
+        }
+    }
+}
