8b132e3c76150e1001880d3942c03688f6ff224b
simon-andersson <simon.andersson@keenswh.com>
2016-05-18 11:45:14 +0200

    Add test skeleton for language output

---
+++ ToyWorld/ToyWorldTests/Language/LanguageOutputTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.ToyWorld.Language;
+using Xunit;
+
+namespace ToyWorldTests.Language
+{
+    class LanguageOutputTests
+    {
+        // Find a vocabulary word given its exact vector
+        [Fact]
+        public void FindWordFromExactVector()
+        {
+        }
+
+        // Find a vocabulary word from nearby vector
+        [Fact]
+        public void FindWordFromApproximateVector()
+        {
+        }
+    }
+}
