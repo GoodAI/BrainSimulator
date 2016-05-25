88488688828ce22d9829212e576fa5d0000af1ba
Michal Vlasák <michal.vlasak@keenswh.com>
2016-05-25 14:25:54 +0200

    ToyWorld - Bed added to testing map

---
+++ ToyWorld/World/GameActors/Tiles/OnGroundInteractable/Bed.cs
@@ -0,0 +1,10 @@
+﻿using VRageMath;
+
+namespace World.GameActors.Tiles.OnGroundInteractable
+{
+    class Bed : DynamicTile
+    {
+        public Bed(ITilesetTable tilesetTable, Vector2I position) : base(tilesetTable, position) { }
+        public Bed(int tileType, Vector2I position) : base(tileType, position) { }
+    }
+}
