5755a104458770dd9156bb224e4cebba0fd25b58
Martin Milota <martin.milota@keenswh.com>
2016-06-02 19:15:25 +0200

    Added PointLightEffect shader and effect

---
+++ ToyWorld/Render/RenderObjects/Effects/PointLightEffect.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Reflection;
+using RenderingBase.RenderObjects.Effects;
+using VRageMath;
+
+namespace Render.RenderObjects.Effects
+{
+    public class PointLightEffect : NoEffect
+    {
+        private enum Uniforms
+        {
+            // Names must correspond to names as defined in the shaders
+            mw,
+            mvp,
+
+            colorIntensity,
+            decay,
+            lightPos,
+        }
+
+
+        public PointLightEffect()
+            : base(typeof(Uniforms), "Post.Smoke.vert", "Light.PointLight.frag")
+        { }
+
+
+        public void ModelWorldUniform(ref Matrix val)
+        {
+            SetUniformMatrix4(base[Uniforms.mw], val);
+        }
+
+        public void ModelViewProjectionUniform(ref Matrix val)
+        {
+            SetUniformMatrix4(base[Uniforms.mvp], val);
+        }
+
+
+        public void ColorIntensityUniform(Vector4 val)
+        {
+            SetUniform4(base[Uniforms.colorIntensity], val);
+        }
+
+        public void DecayUniform(float val)
+        {
+            SetUniform1(base[Uniforms.decay], val);
+        }
+
+        public void LightPosUniform(Vector3 val)
+        {
+            SetUniform3(base[Uniforms.lightPos], val);
+        }
+    }
+}
---
+++ ToyWorld/Render/RenderObjects/Effects/Src/Light/PointLight.frag
@@ -0,0 +1,27 @@
+﻿#version 330
+
+
+// Lighting coefficients
+// Light color and intensity
+uniform vec4 colorIntensity = vec4(vec3(220), 0.5f);
+// The decay rate of the light
+uniform float decay = 50;
+// The light's world position
+uniform vec3 lightPos = vec3(0);
+
+
+smooth in vec3 f_worldPos;
+
+layout(location = 0) out vec4 out_color;
+
+
+void main()
+{
+	vec3 dist = lightPos - f_worldPos;
+
+	float intensity = 1 / (1 + decay * dot(dist, dist));
+
+	out_color = vec4(colorIntensity.xyz, 220);
+	out_color *= intensity * colorIntensity.w;
+	out_color.xyz *= out_color.w; // Pre-multiply by alpha
+}
