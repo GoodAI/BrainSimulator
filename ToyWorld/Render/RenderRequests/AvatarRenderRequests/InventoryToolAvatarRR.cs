fdb16416e4de2772fbb09bb7771303095dba676e
Martin Milota <martin.milota@keenswh.com>
2016-06-07 12:22:10 +0200

    Added InventoryToolARR to display only the tool of the selected avatar

---
+++ ToyWorld/Render/RenderRequests/AvatarRenderRequests/InventoryToolAvatarRR.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using GoodAI.ToyWorld.Control;
+using RenderingBase.Renderer;
+using VRageMath;
+using World.ToyWorldCore;
+
+namespace Render.RenderRequests
+{
+    internal class InventoryToolAvatarRR : AvatarRRBase, IInventoryToolAvatarRR
+    {
+        #region Genesis
+
+        public InventoryToolAvatarRR(int avatarID)
+            : base(avatarID)
+        { }
+
+        #endregion
+
+        #region IInventoryToolAvatarRR overrides
+        #endregion
+
+        #region RenderRequestBase overrides
+
+        public override void Init(RendererBase<ToyWorld> renderer, ToyWorld world)
+        {
+            SizeV = new Vector2(0.9f);
+        }
+
+        public override void Draw(RendererBase<ToyWorld> renderer, ToyWorld world)
+        {
+            var avatar = world.GetAvatar(AvatarID);
+
+            // Compute transform of the center of the inventory
+            const float margin = 0.05f;
+            Vector2 position = Vector2.One - (new Vector2(margin) + SizeV * 0.5f);
+
+            DrawAvatarInventoryTool(renderer, avatar, SizeV, position, InventoryBackgroundType);
+        }
+
+        #endregion
+    }
+}
--- ToyWorld/ToyWorldAPI/Render/IAvatarRenderRequests.cs
+++ ToyWorld/ToyWorldAPI/Render/IAvatarRenderRequests.cs
@@ -53,4 +53,20 @@ namespace GoodAI.ToyWorld.Control
         /// </summary>
         IFovAvatarRR FovAvatarRenderRequest { get; set; }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IInventoryToolAvatarRR : IAvatarRenderRequest
+    {
+        /// <summary>
+        /// This property is not used and its value is undefined.
+        /// </summary>
+        new PointF RelativePosition { get; }
+
+        /// <summary>
+        /// This property is not used and its value is undefined.
+        /// </summary>
+        new bool RotateMap { get; }
+    }
 }
