7abe419a1bc7c9383fd82bcb18a16ace1794379b
Martin Stránský <martin.stransky@keenswh.com>
2016-07-27 10:52:33 +0200

    LTSCT1

---
+++ School/Module/Learning tasks/LTSCT1.cs
@@ -0,0 +1,68 @@
+﻿using GoodAI.Modules.School.Common;
+using GoodAI.Modules.School.Worlds;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace GoodAI.Modules.School.LearningTasks
+{
+    [DisplayName("LTSCT1")]
+    public class Ltsct1 : AbstractLearningTask<RoguelikeWorld>
+    {
+        private readonly Random m_rndGen = new Random();
+        private GameObject m_target;
+        private ScFixPositions positions;
+
+        public Ltsct1() : this(null) { }
+
+        public Ltsct1(SchoolWorld w)
+            : base(w)
+        {
+            TSHints = new TrainingSetHints {
+                { TSHintAttributes.IMAGE_NOISE, 0 },
+                { TSHintAttributes.MAX_NUMBER_OF_ATTEMPTS, 1000000 }
+            };
+
+            TSProgression.Add(TSHints.Clone());
+            TSProgression.Add(TSHintAttributes.IMAGE_NOISE, 1);
+            TSProgression.Add(TSHintAttributes.IMAGE_TEXTURE_BACKGROUND, 0);
+        }
+
+        public override void PresentNewTrainingUnit()
+        {
+            if (LearningTaskHelpers.FlipCoin(m_rndGen))
+            {
+                WrappedWorld.CreateNonVisibleAgent();
+                CreateTarget();
+            }
+            else
+            {
+                m_target = null;
+            }
+
+            positions = new ScFixPositions(WrappedWorld.GetPowGeometry());
+        }
+
+        protected override bool DidTrainingUnitComplete(ref bool wasUnitSuccessful)
+        {
+            bool wasTargetDetected = Math.Abs(SchoolWorld.ActionInput.Host[0]) > 0.001f;
+            bool isTargetPresent = m_target != null;
+            wasUnitSuccessful = (wasTargetDetected == isTargetPresent);
+            return true;
+        }
+
+        protected void CreateTarget()
+        {
+            SizeF size = new SizeF(WrappedWorld.GetPowGeometry().Width / 4, WrappedWorld.GetPowGeometry().Height / 4);
+
+            PointF location = positions.GetRandomPosition(m_rndGen);
+            m_target = WrappedWorld.CreateShape(Shape.GetRandomShape(m_rndGen, 8), Color.White, location, size);
+            // Plumber:
+            //m_target.X = m_rndGen.Next(0, World.Scene.Width - m_target.Width + 1);
+            //m_target.Y = World.Scene.Height - m_target.Height;
+            // Roguelike:
+            //m_target.X = m_rndGen.Next(0, WrappedWorld.Viewport.Width - m_target.Width + 1);
+            //m_target.Y = m_rndGen.Next(0, WrappedWorld.Viewport.Height - m_target.Height + 1);
+        }
+    }
+}
---
+++ School/Module/Learning tasks/ScFixPositions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace GoodAI.Modules.School.LearningTasks
+{
+    public class ScFixPositions
+    {
+        public List<float[]> Positions;
+
+        public ScFixPositions(RectangleF powGeometry)
+        {
+            Positions = new List<float[]>
+            {
+                new[] {0.0f, 0.0f},
+                new[] {0.0f, 1.0f},
+                new[] {0.0f, 2.0f},
+                new[] {1.0f, 0.0f},
+                new[] {1.0f, 2.0f},
+                new[] {2.0f, 0.0f},
+                new[] {2.0f, 1.0f},
+                new[] {2.0f, 2.0f}
+            };
+
+            foreach (float[] position in Positions)
+            {
+                float marginX = powGeometry.Width / 16;
+                float marginY = powGeometry.Height / 16;
+                position[0] = marginX + position[0] * ((powGeometry.Width - marginX) / 3) + powGeometry.X;
+                position[1] = marginY + position[1] * ((powGeometry.Height - marginY) / 3) + powGeometry.Y;
+            }
+        }
+
+        public PointF GetRandomPosition(Random rnd)
+        {
+            int r = rnd.Next(Positions.Count);
+            var p = Positions[r];
+            return new PointF(p[0],p[1]);
+        }
+    }
+}
