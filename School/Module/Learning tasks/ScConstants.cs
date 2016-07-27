491462aadba670ed433e1d9267edc3ccc8733aeb
Martin Poliak <martin.poliak@keenswh.com>
2016-07-27 17:37:06 +0200

    LT2 - 2 shapes, LT3 - color. Added constants, added inheritance.

---
+++ School/Module/Learning tasks/ScConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using ManagedCuda.NPP;
+
+namespace GoodAI.Modules.School.LearningTasks
+{
+    public class ScConstants
+    {
+        public static int numPositions = 9;
+        public static int numShapes = 8;
+        public static int numColors = 4;
+    }
+}
