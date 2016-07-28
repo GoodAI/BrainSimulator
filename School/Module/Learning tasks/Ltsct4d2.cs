2ba59798bdc967e69b05abdee623f29b77133380
Martin Poliak <martin.poliak@keenswh.com>
2016-07-28 19:30:03 +0200

    Fix of actions pairing to bmps.

--- School/Module/Learning tasks/Ltsct4d2.cs
+++ School/Module/Learning tasks/Ltsct4d2.cs
@@ -44,8 +44,8 @@ namespace GoodAI.Modules.School.LearningTasks
             {
                 WrappedWorld.CreateRandomStone(location2, size, m_rndGen);
             }
-            
-            Actions.WriteActions(StreamWriter);string joinedActions = Actions.ToString();MyLog.INFO.WriteLine(joinedActions);
+
+            WriteActions();
 
         }
     }
