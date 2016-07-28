201624b2e3a91f702b8b4b9ee4fd102091e46382
Martin Stránský <martin.stransky@keenswh.com>
2016-07-28 17:43:34 +0200

    path fixes

--- School/Module/Learning tasks/Ltsct5.cs
+++ School/Module/Learning tasks/Ltsct5.cs
@@ -15,7 +15,7 @@ namespace GoodAI.Modules.School.LearningTasks
 
         public override string Path
         {
-            get { return @"D:\summerCampSamples\SCT2\"; }
+            get { return @"D:\summerCampSamples\SCT5\"; }
         }
 
         public Ltsct5() : this(null) { }
