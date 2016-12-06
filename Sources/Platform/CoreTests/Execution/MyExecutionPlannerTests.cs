bae5d30641febf340f68f0ba24b85e260d086082
Premek Paska <premysl.paska@keenswh.com>
2016-12-06 15:44:20 +0700

    Add a test for execution planner not respecting the task order.

---
+++ Sources/Platform/CoreTests/Execution/MyExecutionPlannerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.Core.Execution;
+using GoodAI.Core.Nodes;
+using GoodAI.Core.Task;
+using GoodAI.Core.Utils;
+using Xunit;
+
+namespace CoreTests.Execution
+{
+    public class MyExecutionPlannerTests
+    {
+        [Fact]
+        public void PlannerRespectsTaskOrder()
+        {
+            var planner = new MyDefaultExecutionPlanner();
+            
+            var taskOrderNode = new TaskOrderNode();
+
+            MyNodeInfo.CollectNodeInfo(typeof(TaskOrderNode));
+
+            MyExecutionBlock execBlock = planner.CreateNodeExecutionPlan(taskOrderNode, initPhase: false);
+           
+            var lastOrder = -1;
+            foreach (IMyExecutable task in execBlock.Children)
+            {
+                var taskInfo = task.GetType().GetCustomAttribute<MyTaskInfoAttribute>(true);
+                if (taskInfo == null)
+                    continue;
+
+                Assert.True(lastOrder <= taskInfo.Order,
+                    "Task order must be greater or equal to the previous task order");
+
+                lastOrder = taskInfo.Order;
+            }
+        }
+    }
+}
---
+++ Sources/Platform/CoreTests/Execution/TaskOrderNode.cs
@@ -0,0 +1,41 @@
+﻿using GoodAI.Core.Nodes;
+using GoodAI.Core.Task;
+using GoodAI.Core.Utils;
+
+namespace CoreTests.Execution
+{
+    /// <summary>
+    /// A testing node that uses task ordering by attributes.
+    /// </summary>
+    internal sealed class TaskOrderNode : MyWorkingNode
+    {
+        public FooTask FooTaskProp { get; private set; }
+        public BarTask BarTaskProp { get; private set; }
+        public ZedTask ZedTaskProp { get; private set; }
+
+        [MyTaskInfo(Order = 0)]
+        public class ZedTask : MyTask<TaskOrderNode>
+        {
+            public override void Init(int nGPU) { }
+            public override void Execute() { }
+        }
+
+        [MyTaskInfo(Order = 1)]
+        public class FooTask : MyTask<TaskOrderNode>
+        {
+            public override void Init(int nGPU) { }
+            public override void Execute() { }
+        }
+
+        [MyTaskInfo(Order = 2)]
+        public class BarTask : MyTask<TaskOrderNode>
+        {
+            public override void Init(int nGPU) { }
+            public override void Execute() { }
+        }
+
+        public override void UpdateMemoryBlocks()
+        {
+        }
+    }
+}
