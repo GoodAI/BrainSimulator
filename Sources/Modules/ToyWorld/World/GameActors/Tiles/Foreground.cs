8fb12d611539006f765f898d051e9e32e0c57486
Martin Stránský <martin.stransky@keenswh.com>
2017-01-03 18:00:00 +0100

    Tile foreground added

---
+++ Sources/Modules/ToyWorld/World/GameActors/Tiles/Foreground.cs
@@ -0,0 +1,13 @@
+﻿namespace World.GameActors.Tiles.Background
+{
+    public class Foreground : StaticTile
+    {
+        public Foreground() : base (){ } 
+
+ 		public Foreground(int textureId) : base(textureId) { }
+
+        public Foreground(string textureName) : base(textureName)
+        {
+        }
+    }
+}
