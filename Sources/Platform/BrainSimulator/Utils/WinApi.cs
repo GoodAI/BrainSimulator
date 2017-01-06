ea6229777e12720d5f785c5aa9367aa6bef9d70e
Premek Paska <premysl.paska@keenswh.com>
2017-01-06 13:03:54 +0700

    WinApi: Import Win32 APIs Get- / SetWindowPlacement, add required types.

---
+++ Sources/Platform/BrainSimulator/Utils/WinApi.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+// Some code adapted from www.pinvoke.net
+
+namespace GoodAI.BrainSimulator.Utils
+{
+    /// <summary>
+    /// Wrapper around the Winapi POINT type.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>Implicit cast.</summary>
+        public static implicit operator Point(POINT p)
+        {
+            return new Point(p.X, p.Y);
+        }
+
+        /// <summary> Implicit cast.</summary>
+        public static implicit operator POINT(Point p)
+        {
+            return new POINT(p.X, p.Y);
+        }
+    }
+
+    /// <summary>
+    /// Wrapper around the Winapi RECT type.
+    /// </summary>
+    [Serializable, StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        public int Height => Bottom - Top;
+        public int Width => Right - Left;
+        public Size Size => new Size(Width, Height);
+        public Point Location => new Point(Left, Top);
+
+        /// <summary>Convert RECT to a Rectangle.</summary>
+        public Rectangle ToRectangle()
+        {
+            return Rectangle.FromLTRB(Left, Top, Right, Bottom);
+        }
+
+        /// <summary>Convert Rectangle to a RECT</summary>
+        public static RECT FromRectangle(Rectangle rectangle)
+        {
+            return new RECT(rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Bottom);
+        }
+
+        #region Implicit casts
+
+        /// <summary>Implicit Cast.</summary>
+        public static implicit operator Rectangle(RECT rect)
+        {
+            return Rectangle.FromLTRB(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
+
+        /// <summary>Implicit Cast.</summary>
+        public static implicit operator RECT(Rectangle rect)
+        {
+            return new RECT(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
+
+        #endregion
+    }
+
+    public static class WinApi
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        private struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public POINT ptMinPosition;
+            public POINT ptMaxPosition;
+            public RECT rcNormalPosition;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPlacement(IntPtr hWnd, [In] ref WINDOWPLACEMENT lpwndpl);
+    }
+}
