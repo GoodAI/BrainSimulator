b50c9ce06ed3a62d58fbafd8c49c501aabbf9f18
Matěj Nikl <matej.nikl@goodai.com>
2017-01-26 22:31:01 +0100

    Add conversion from projects using old MNISTWorld

--- Sources/Modules/MNIST/Module/Versioning/MyConversion.cs
+++ Sources/Modules/MNIST/Module/Versioning/MyConversion.cs
@@ -6,10 +6,7 @@ namespace MNIST.Versioning
 {
     public class MyConversion : MyBaseConversion
     {
-        public override int CurrentVersion
-        {
-            get { return 2; }
-        }
+        public override int CurrentVersion => 3;
 
         /// <summary>
         /// Convert MNISTWorld brains for the training and testing tasks. 
@@ -33,6 +30,183 @@ namespace MNIST.Versioning
             }
 
             return document.ToString();
+        }
+
+        public static string Convert2To3(string xml)
+        {
+
+            XDocument document = XDocument.Parse(xml);
+
+            foreach (XElement worldNode in document.Root.Descendants("World"))
+            {
+                XAttribute worldAtt = worldNode.Attribute(GetRealTypeAttributeName());
+
+                if (worldAtt.Value == "MNIST.MyMNISTWorld")
+                {
+                    worldAtt.Value = "MNIST.MNISTWorld";
+
+                    // one-hot encoding conversion
+                    #region
+                    foreach (XElement node in worldNode.Descendants("Binary"))
+                    {
+                        node.Name = "OneHot";
+                    }
+                    #endregion
+
+                    // image binarize conversion
+                    #region
+                    foreach (XElement node in worldNode.Descendants("BinaryPixels"))
+                    {
+                        node.Name = "Binarize";
+                    }
+                    #endregion
+
+                    int? trainExamplesPerClass = null;
+                    int? testExamplesPerClass = null;
+
+                    // Get number of examples per class from Init task
+                    #region 
+                    foreach (XElement taskNode in worldNode.Descendants("Task"))
+                    {
+                        XAttribute taskAtt = taskNode.Attribute(GetRealTypeAttributeName());
+                        if (taskAtt.Value == "MNIST.MyInitMNISTTask")
+                        {
+                            foreach (XElement node in taskNode.Descendants("TrainingExamplesPerDigit"))
+                            {
+                                trainExamplesPerClass = int.Parse(node.Value);
+                            }
+
+                            foreach (XElement node in taskNode.Descendants("TestExamplesPerDigit"))
+                            {
+                                testExamplesPerClass = int.Parse(node.Value);
+                            }
+                            taskNode.Remove();
+                            break;
+                        }
+                    }
+                    #endregion
+
+                    foreach (XElement taskNode in worldNode.Descendants("Task"))
+                    {
+                        bool isTrain = false;
+                        XAttribute taskAtt;
+
+                        // task's attributes renaming + determine if current task is training task - set in isTrain
+                        #region 
+                        taskAtt = taskNode.Attribute(GetRealTypeAttributeName());
+                        if (taskAtt.Value == "MNIST.MySendTrainingMNISTTask")
+                        {
+                            taskAtt.Value = "MNIST.SendMNISTTrainDataTask";
+                            isTrain = true;
+                        }
+                        else if (taskAtt.Value == "MNIST.MySendTestMNISTTask")
+                        {
+                            taskAtt.Value = "MNIST.SendMNISTTestDataTask";
+                        }
+
+                        taskAtt = taskNode.Attribute("PropertyName");
+                        if (taskAtt.Value == "SendTrainingMNISTData")
+                        {
+                            taskAtt.Value = "SendMNISTTrainData";
+                        }
+                        else if (taskAtt.Value == "SendTestMNISTData")
+                        {
+                            taskAtt.Value = "SendMNISTTestData";
+                        }
+                        #endregion
+
+                        // class filter conversion
+                        #region
+                        bool useClassFilter = false;
+                        foreach (XElement node in taskNode.Descendants("SendNumbers"))
+                        {
+                            if (node.Value == "All")
+                            {
+                                node.Remove();
+                                useClassFilter = false;
+                                break;
+                            }
+                            else
+                            {
+                                node.Name = "ClassFilter";
+                                useClassFilter = true;
+                                break;
+                            }
+
+                        }
+                        taskNode.Add(new XElement("UseClassFilter", useClassFilter));
+                        #endregion
+
+                        // class order conversion
+                        #region
+                        foreach (XElement node in taskNode.Descendants("SequenceOrdered"))
+                        {
+                            node.Name = "ClassOrder";
+                            if (node.Value == "True")
+                            {
+                                node.Value = ClassOrderOption.Increasing.ToString();
+                            }
+                            else
+                            {
+                                node.Value = ClassOrderOption.Random.ToString();
+                            }
+                        }
+                        #endregion
+
+                        // example order conversion (from the train task)
+                        #region
+                        foreach (XElement node in taskNode.Descendants("RandomEnumerate"))
+                        {
+                            ExampleOrderOption exampleOrder; 
+                            if (node.Value == "True")
+                            {
+                                exampleOrder = ExampleOrderOption.Shuffle;
+                            }
+                            else
+                            {
+                                exampleOrder = ExampleOrderOption.NoShuffle;
+                            }
+
+                            if (isTrain)
+                            {
+                                worldNode.Add(new XElement("ExampleOrder", exampleOrder));
+                            }
+
+                            node.Remove();
+                            break;
+                        }
+                        #endregion
+
+                        // examples per class conversion (from previously read values from Init task)
+                        #region
+                        if (isTrain)
+                        {
+                            if (trainExamplesPerClass != null)
+                            {
+                                taskNode.Add(new XElement("ExamplesPerClass", trainExamplesPerClass));
+                            }
+                        }
+                        else
+                        {
+                            if (trainExamplesPerClass != null)
+                            {
+                                taskNode.Add(new XElement("ExamplesPerClass", testExamplesPerClass));
+                            }
+
+                        }
+                        #endregion
+                    }
+                }
+            }
+
+            return document.ToString();
+        }
+
+        private static XName GetRealTypeAttributeName()
+        {
+            XNamespace yaxlib = "http://www.sinairv.com/yaxlib/";
+            XName realtype = yaxlib + "realtype";
+            return realtype;
         }
     }
 }
