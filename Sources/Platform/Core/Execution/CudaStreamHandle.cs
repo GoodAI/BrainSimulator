0c36103e23d03c1d708ef01b9e6dc8f0da2d143b
Premek Paska <premysl.paska@goodai.com>
2017-03-15 16:23:35 +0100

    Inject CudaStreamHandle into new kernels.

---
+++ Sources/Platform/Core/Execution/CudaStreamHandle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagedCuda;
+
+namespace GoodAI.Core.Execution
+{
+    internal class CudaStreamHandle
+    {
+        // TODO: with multi-threading the stream will need to be injected and stored here (another option is a thread-local storage)
+        public CudaStream CurrentStream => m_streamProvider.CurrentStream;
+
+        private readonly CudaStreamProvider m_streamProvider;
+
+        public CudaStreamHandle(CudaStreamProvider source)
+        {
+            m_streamProvider = source;
+        }
+    }
+}
---
+++ Sources/Platform/Core/Execution/CudaStreamProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagedCuda;
+using ManagedCuda.BasicTypes;
+
+namespace GoodAI.Core.Execution
+{
+    internal class CudaStreamProvider
+    {
+        public static CudaStreamProvider Instance { get; } = new CudaStreamProvider();
+
+        public CudaStream CurrentStream { get; } = new CudaStream(CUstream.StreamPerThread);
+
+        private readonly int StreamsPerThread = 8;
+
+        public void SwitchToNextStream()
+        {
+            // TODO
+        }
+    }
+}
